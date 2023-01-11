a98cb79a5e2319b1c95b48179488c47f82c66f82
Todor Borisov <todor0203@gmail.com>
2023-01-11 13:04:27 +0200

    Add new problems

---
+++ 03. JS Advanced - Jan 2023/01. Syntax, Functions and Statements/Lab/02. String Length.js
@@ -0,0 +1,8 @@
+function stringLength(a, b, c) {
+
+    let totalLength = a.length + b.length + c.length;
+    console.log(totalLength);
+    console.log(Math.floor(totalLength / 3));
+
+}
+stringLength('chocolate', 'ice cream', 'cake')

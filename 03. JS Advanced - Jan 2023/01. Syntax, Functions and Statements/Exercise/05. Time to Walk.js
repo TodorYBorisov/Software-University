8bb6acd8a782b3d8a70b874d8e7b983609705226
Todor Borisov <todor0203@gmail.com>
2023-01-15 21:48:08 +0200

    Update 05. Time to Walk.js

--- 03. JS Advanced - Jan 2023/01. Syntax, Functions and Statements/Exercise/05. Time to Walk.js
+++ 03. JS Advanced - Jan 2023/01. Syntax, Functions and Statements/Exercise/05. Time to Walk.js
@@ -7,7 +7,7 @@ function timeToWalk(steps, footprint, speed) {
 
     let hours = Math.floor(time / 3600);
     let minutes = Math.floor(time / 60);
-    let seconds = Math.floor(time % 60);
+    let seconds = Math.round(time % 60);
 
     console.log(`${hours < 10 ? '0' : ''}${hours}:${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`);
 

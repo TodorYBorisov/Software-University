df929ea7b7926f9410bf01f01e01bfaf1fb8a53c
Todor Borisov <todor0203@gmail.com>
2023-01-16 19:16:40 +0200

    Update 02. Print every N-th Element from an Array.js

--- 03. JS Advanced - Jan 2023/02. Arrays and Nested Arrays/Lab/02. Print every N-th Element from an Array.js
+++ 03. JS Advanced - Jan 2023/02. Arrays and Nested Arrays/Lab/02. Print every N-th Element from an Array.js
@@ -1,11 +1,11 @@
 function printEvryNthElemnt(array,step) {
 
     let res = [];
-    for (let i = 0; i < array.length; i+=2) {
+    for (let i = 0; i < array.length; i+=step) {
         const element = array[i];
         res.push(element);
     }
-    console.log(res);
+    return res;
 }
 printEvryNthElemnt(['5', 
 '20', 

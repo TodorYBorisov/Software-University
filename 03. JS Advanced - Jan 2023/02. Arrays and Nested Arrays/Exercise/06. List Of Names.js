24483ae02836837fe046cd2488d7c97d8afa32da
Todor Borisov <todor0203@gmail.com>
2023-01-16 20:56:50 +0200

    Create 06. List Of Names.js

---
+++ 03. JS Advanced - Jan 2023/02. Arrays and Nested Arrays/Exercise/06. List Of Names.js
@@ -0,0 +1,16 @@
+function listOfNames(array) {
+
+    let sortedArray = array.sort((a, b) => a.localeCompare(b));
+
+
+for (let i = 0; i < sortedArray.length; i++) {
+ 
+    console.log(`${i+1}.${sortedArray[i]}`);
+}
+
+    // sortedArray.forEach((element,i) => {
+    //     console.log(`${i+1}.${element}`);
+
+    // });
+}
+listOfNames(["John", "Bob", "Christina", "Ema"]);

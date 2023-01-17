1c70dbb1a4f1c7b4e0ec9f9de664fe4d045f4ed3
Todor Borisov <todor0203@gmail.com>
2023-01-17 11:29:50 +0200

    Update 08. Sort an Array by 2 Criteria.js

--- 03. JS Advanced - Jan 2023/02. Arrays and Nested Arrays/Exercise/08. Sort an Array by 2 Criteria.js
+++ 03. JS Advanced - Jan 2023/02. Arrays and Nested Arrays/Exercise/08. Sort an Array by 2 Criteria.js
@@ -2,8 +2,10 @@ function sortAnArrayBy2Criteria(array) {
 
     let sort = array.sort((a, b) => a.length - b.length || a.localeCompare(b));
 
-    for (const element of sort) {
-        console.log(element);
-    }
+    // for (const element of sort) {
+    //     console.log(element);
+    // }
+
+    console.log(sort.join('\n'));
 }
 sortAnArrayBy2Criteria(['Isacc', 'Theodor', 'Jack', 'Harrison', 'George']);

8d260146ac0edd963933af5453e6e72225ee73b8
Todor Borisov <todor0203@gmail.com>
2023-01-17 11:51:23 +0200

    Update 09. Magic Matrices.js

--- 03. JS Advanced - Jan 2023/02. Arrays and Nested Arrays/Exercise/09. Magic Matrices.js
+++ 03. JS Advanced - Jan 2023/02. Arrays and Nested Arrays/Exercise/09. Magic Matrices.js
@@ -1,15 +1,24 @@
 function magicMatrices(array) {
 
-let sumRows=0;
-let sumCols=0;
+    let sumRows = 0;
+    let sumCols = 0;
 
-for (let i = 0; i < array.length; i++) {
-    let currentArray = array[i].join('');
-    console.log(currentArray);
-    
+    for (let i = 0; i < array.length; i++) {
+
+        let currentArray = array[i].join('');
+
+        for (let j = 0; j < currentArray.length; j++) {
+            sumRows += Number(currentArray[j]);
+            sumCols += Number(currentArray[0]);
+        }
+    }
+
+    if (sumRows === sumCols) {
+        console.log('true');
+    } else {
+        console.log('false');
+    }
 }
-
-    
-}magicMatrices([[4, 5, 6],
-                [6, 5, 4],
-                [5, 5, 5]]);
+magicMatrices([[4, 5, 6],
+[6, 5, 4],
+[5, 5, 5]]);

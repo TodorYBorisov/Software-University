d8607f2398a6f8e5f488d820c666da3be75617da
Todor Borisov <todor0203@gmail.com>
2023-01-17 11:29:25 +0200

    Create 09. Magic Matrices.js

---
+++ 03. JS Advanced - Jan 2023/02. Arrays and Nested Arrays/Exercise/09. Magic Matrices.js
@@ -0,0 +1,15 @@
+function magicMatrices(array) {
+
+let sumRows=0;
+let sumCols=0;
+
+for (let i = 0; i < array.length; i++) {
+    let currentArray = array[i].join('');
+    console.log(currentArray);
+    
+}
+
+    
+}magicMatrices([[4, 5, 6],
+                [6, 5, 4],
+                [5, 5, 5]]);

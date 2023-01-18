1a68d837e3a3b0baec83273c5d5dddef9ef1e8c7
Todor Borisov <todor0203@gmail.com>
2023-01-18 22:14:30 +0200

    Create 05. Smallest Two Numbers.js

---
+++ 03. JS Advanced - Jan 2023/02. Arrays and Nested Arrays/Lab/05. Smallest Two Numbers.js
@@ -0,0 +1,10 @@
+function smallestTwoNumbers(array) {
+
+    let sort = array.sort((a,b)=>a-b).slice(0,2);
+    //let smallestTwo =sort.slice(0,2);
+    console.log(sort.join(' '));
+
+
+}
+smallestTwoNumbers([30, 15, 50, 5]);
+smallestTwoNumbers([3, 0, 10, 4, 7, 3]);

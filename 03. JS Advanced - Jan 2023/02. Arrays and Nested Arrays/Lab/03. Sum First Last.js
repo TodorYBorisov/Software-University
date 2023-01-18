045fd05db4de2529c0f1fd78df9a3ddedc2d6968
Todor Borisov <todor0203@gmail.com>
2023-01-18 21:38:19 +0200

    Create 03. Sum First Last.js

---
+++ 03. JS Advanced - Jan 2023/02. Arrays and Nested Arrays/Lab/03. Sum First Last.js
@@ -0,0 +1,11 @@
+function sumFirstLast(array) {
+
+    // let firstNumber = Number(array[0]);
+    // let lastNumber = Number(array[array.length - 1]);
+    // let result = firstNumber + lastNumber;
+
+    console.log(Number(array[0])+Number(array[array.length - 1]));
+
+}
+sumFirstLast(['20', '30', '40']);
+sumFirstLast(['5', '10']);

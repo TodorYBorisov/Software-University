825a943e2210377fce4f04b3ec65bacfd080fef7
Todor Borisov <todor0203@gmail.com>
2023-01-19 00:42:05 +0200

    Update 06. Bigger Half.js

--- 03. JS Advanced - Jan 2023/02. Arrays and Nested Arrays/Lab/06. Bigger Half.js
+++ 03. JS Advanced - Jan 2023/02. Arrays and Nested Arrays/Lab/06. Bigger Half.js
@@ -1,12 +1,10 @@
 function biggerHalf(array) {
 
     let sorted = array.sort((a, b) => a - b);
-
     let startIndex = Math.floor(sorted.length / 2);
+    let arrayHalf = sorted.splice(startIndex);
 
-    let arrayHalf= sorted.splice(startIndex);
-
-    console.log(arrayHalf);
+     return arrayHalf;
 }
 biggerHalf([4, 7, 2, 5]);
 biggerHalf([3, 19, 14, 7, 2, 19, 6]);

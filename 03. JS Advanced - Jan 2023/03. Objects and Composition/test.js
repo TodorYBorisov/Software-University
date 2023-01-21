8671c7f5fe9231ad82136a42195cce081253ff49
Todor Borisov <todor0203@gmail.com>
2023-01-21 18:59:25 +0200

    Arrange

---
+++ 03. JS Advanced - Jan 2023/03. Objects and Composition/test.js
@@ -0,0 +1,16 @@
+function test() {
+
+    let temp = 20;
+    let tempSettings = 21;
+
+
+    if (temp < tempSettings) {
+        temp += 1;
+    } else if (temp > tempSettings) {
+        temp -= 1;
+    } else {
+
+    }
+
+}
+test();

253889c0f1f1322a33d3b7bfd427852c976d313b
Todor Borisov <todor0203@gmail.com>
2023-01-22 19:35:10 +0200

    Create 06. From JSON to HTML Table Not mine 1.js

---
+++ 03. JS Advanced - Jan 2023/03. Objects and Composition/Lab/06. From JSON to HTML Table Not mine 1.js
@@ -0,0 +1,23 @@
+function fromJSONToHTMLTable(dataAsJson) {
+    const result = [];
+    result.push('<table>');
+    const object = JSON.parse(dataAsJson);
+
+    const keys = Object.keys(object[0]);
+
+    result.push(`    <tr>${keys.map(k => `<th>${k}</th>`).join('')}</tr>`);
+
+
+    for (const entry of object) {
+        result.push(`    <tr>${keys.map(k => `<td>${entry[k]}</td>`).join('')}</tr> `);
+    }
+
+    result.push('</table>');
+
+    return result.join('\n');
+}
+console.log(fromJSONToHTMLTable(`[{"Name":"Stamat",
+"Score":5.5},
+{"Name":"Rumen",
+"Score":6}]`
+));

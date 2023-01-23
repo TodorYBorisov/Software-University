66b21129a2e621639c48c2eb1f6c08a56983e47b
Todor Borisov <todor0203@gmail.com>
2023-01-23 17:14:36 +0200

    Create 05. Lowest Prices in Cities.js

---
+++ 03. JS Advanced - Jan 2023/03. Objects and Composition/Exercise/05. Lowest Prices in Cities.js
@@ -0,0 +1,32 @@
+function lowestPriceinCities(array) {
+
+    let result = {};
+
+    for (let line of array) {
+
+        let [townName, productName, productPrice] = line.split(' | ');
+
+        productPrice = Number(productPrice);
+
+        if (!result[productName]) {
+            result[productName] = { productPrice, townName };
+        } else {
+            if (result[productName].productPrice > productPrice) {
+                result[productName] = { productPrice, townName };
+            }
+        }
+    }
+
+for (let key in result) {
+
+    console.log(`${key} -> ${result[key].productPrice} (${result[key].townName})`);
+   
+}
+
+} lowestPriceinCities(['Sample Town | Sample Product | 1000',
+    'Sample Town | Orange | 2',
+    'Sample Town | Peach | 1',
+    'Sofia | Orange | 3',
+    'Sofia | Peach | 2',
+    'New York | Sample Product | 1000.1',
+    'New York | Burger | 10']);

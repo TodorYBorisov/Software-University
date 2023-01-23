62dbcad3b13cdc73cbf5cb13c567cf04f0e98d42
Todor Borisov <todor0203@gmail.com>
2023-01-24 00:54:29 +0200

    Create 07. Towns to JSON.js

---
+++ 03. JS Advanced - Jan 2023/03. Objects and Composition/Exercise/07. Towns to JSON.js
@@ -0,0 +1,27 @@
+function townsToJson(array) {
+
+    let res = [];
+
+    for (let i = 1; i < array.length; i++) {
+        let [town, latitude, longitude] = array[i]
+            .split('|')
+            .filter((x) => x.length !== 0)
+            .map((el) => el.trim());
+
+        res.push({
+            Town: town,
+            Latitude: Number(latitude).toFixed(2),
+            Longitude: Number(longitude).toFixed(2),
+        });
+    }
+
+console.log(JSON.stringify(res));
+
+
+}
+townsToJson(['| Town | Latitude | Longitude |',
+    '| Sofia | 42.696552 | 23.32601 |',
+    '| Beijing | 39.913818 | 116.363625 |']);
+townsToJson(['| Town | Latitude | Longitude |',
+    '| Veliko Turnovo | 43.0757 | 25.6172 |',
+    '| Monatevideo | 34.50 | 56.11 |']);

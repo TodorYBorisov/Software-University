17d8507b2f66002c809d74cb5caccc6ee9dc6bd6
Todor Borisov <todor0203@gmail.com>
2023-01-26 14:58:38 +0200

    Update EditElement.js

--- 03. JS Advanced - Jan 2023/04. DOM Introduction/Lab/01.Edit Element/EditElement.js
+++ 03. JS Advanced - Jan 2023/04. DOM Introduction/Lab/01.Edit Element/EditElement.js
@@ -1,7 +1,7 @@
 function editElement(ref, match, replacer) {
 
-    let text = ref.textContent;                    // Взимаме в променлива текста от неговата референция
-    let result = text.split(match).join(replacer); // Правим нова променлива'резултат'разделяме текста по съвпадението и го събираме по риплейсъра 
-    ref.textContent = result;                      // Референцията на текста я даваме да е равна на резултата 
+    let text = ref.textContent;                    // Взимаме в променлива текста от неговата референция.
+    let result = text.split(match).join(replacer); // Правим нова променлива'резултат'разделяме текста по съвпадението и го събираме по риплейсъра .
+    ref.textContent = result;                      // Референцията на текста я даваме да е равна на резултата.
 }
 editElement();

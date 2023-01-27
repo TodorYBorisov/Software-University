b8a4ffaade48bc1be8e30cc6ba94f9b8c1038e7f
Todor Borisov <todor0203@gmail.com>
2023-01-27 17:22:23 +0200

    Update ColorizeTable.js

--- 03. JS Advanced - Jan 2023/04. DOM Introduction/Lab/05.Colorize Table/ColorizeTable.js
+++ 03. JS Advanced - Jan 2023/04. DOM Introduction/Lab/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,23 @@
 function colorize() {
-    // TODO
+
+    // const rows = document.querySelectorAll('table tr');
+
+    // for (let i = 0; i < rows.length; i++) {
+    //     const row = rows[i];
+
+    //     if (i % 2 !== 0) {
+    //         row.style.background = 'teal';
+    //     }
+    // }
+
+    const rows = document.querySelectorAll('table tr:nth-of-type(even)');
+
+    // for (const row of rows) {
+    //     row.style.backgroundColor = 'teal';
+    // }
+    rows.forEach(element => {
+        element.style.backgroundColor = 'teal';
+
+    });
+
 }

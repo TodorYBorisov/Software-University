d18244ef0a7518e2dfa853ede965e427c3750715
Todor Borisov <todor0203@gmail.com>
2023-01-27 14:34:48 +0200

    Update ShowMore.js

--- 03. JS Advanced - Jan 2023/04. DOM Introduction/Lab/04.Show More/ShowMore.js
+++ 03. JS Advanced - Jan 2023/04. DOM Introduction/Lab/04.Show More/ShowMore.js
@@ -1,3 +1,7 @@
 function showText() {
-    // TODO
+
+    const readMore = document.getElementById('more');
+    readMore.style.display = 'none';
+    const spanElement = document.getElementById('text');
+    spanElement.style.display = 'inline';
 }

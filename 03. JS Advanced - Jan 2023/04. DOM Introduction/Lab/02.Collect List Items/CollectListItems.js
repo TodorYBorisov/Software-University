5ce8753b3ea07b33415c9191a1f6b57270952b55
Todor Borisov <todor0203@gmail.com>
2023-01-27 11:44:49 +0200

    Update CollectListItems.js

--- 03. JS Advanced - Jan 2023/04. DOM Introduction/Lab/02.Collect List Items/CollectListItems.js
+++ 03. JS Advanced - Jan 2023/04. DOM Introduction/Lab/02.Collect List Items/CollectListItems.js
@@ -1,8 +1,12 @@
 function extractText() {
-    const colectItems = document.querySelectorAll('ul li');
-    const result = document.getElementById('result');
+    // const colectItems = document.querySelectorAll('ul li');
+    // const result = document.getElementById('result');
 
-    for (const node of colectItems) {
-        result.value += node.textContent + '\n';
-    }
+    // for (const node of colectItems) {
+    //     result.value += node.textContent + '\n';
+    // }
+
+    const ulElements = document.getElementById('items');
+    const textArea = document.getElementById('result');
+    textArea.value = ulElements.textContent;
 }

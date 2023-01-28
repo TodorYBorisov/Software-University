34cc1348de6365d23e1358850b540d412dc30dba
Todor Borisov <todor0203@gmail.com>
2023-01-28 17:50:15 +0200

    Update SumTable.js

--- 03. JS Advanced - Jan 2023/04. DOM Introduction/Lab/06.Sum Table/SumTable.js
+++ 03. JS Advanced - Jan 2023/04. DOM Introduction/Lab/06.Sum Table/SumTable.js
@@ -1,3 +1,15 @@
 function sumTable() {
 
+    const rows = document.querySelectorAll('td:last-child');
+
+    let arrRows = Array.from(rows);
+
+    let lastElement = arrRows.pop();
+
+    let sum = 0;
+    for (const num of arrRows) {
+        sum += Number(num.textContent);
+    }
+    const total = document.getElementById('sum');
+    total.textContent =Number(sum);
 }

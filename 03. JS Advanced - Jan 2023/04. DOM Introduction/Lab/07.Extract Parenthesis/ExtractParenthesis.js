ce3f95376c0aeb2a96479865d9de04c128fae304
Todor Borisov <todor0203@gmail.com>
2023-01-28 17:50:26 +0200

    Update ExtractParenthesis.js

--- 03. JS Advanced - Jan 2023/04. DOM Introduction/Lab/07.Extract Parenthesis/ExtractParenthesis.js
+++ 03. JS Advanced - Jan 2023/04. DOM Introduction/Lab/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,18 @@
 function extract(content) {
 
+    let p = document.getElementById('content').textContent;
+    
+    let pattern = /\(([^)]+)\)/gm;
+
+    let match = pattern.exec(p);
+
+    let result = [];
+    while (match) {
+
+        result.push(match[1]);
+
+        match = pattern.exec(p);
+    }
+
+    return result.join(', ');
 }

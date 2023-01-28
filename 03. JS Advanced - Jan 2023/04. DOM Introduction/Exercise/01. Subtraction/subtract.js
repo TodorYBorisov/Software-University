4650d237da3815b4418c9809fa39be2f0c356646
Todor Borisov <todor0203@gmail.com>
2023-01-28 18:31:43 +0200

    Update subtract.js

--- 03. JS Advanced - Jan 2023/04. DOM Introduction/Exercise/01. Subtraction/subtract.js
+++ 03. JS Advanced - Jan 2023/04. DOM Introduction/Exercise/01. Subtraction/subtract.js
@@ -3,7 +3,7 @@ function subtract() {
     let firstNumber = document.getElementById('firstNumber').value;
     let secondNumber = document.getElementById('secondNumber').value;
 
-    let subtract  = Number(firstNumber) - Number(secondNumber);
+    let subtract = Number(firstNumber) - Number(secondNumber);
 
     document.getElementById('result').textContent = subtract;
 

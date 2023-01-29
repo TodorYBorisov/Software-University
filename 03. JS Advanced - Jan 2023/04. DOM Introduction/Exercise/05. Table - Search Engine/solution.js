f0914dbb1403ffc8ac655e963301477042e9eac2
Todor Borisov <todor0203@gmail.com>
2023-01-29 14:25:58 +0200

    Update solution.js

--- 03. JS Advanced - Jan 2023/04. DOM Introduction/Exercise/05. Table - Search Engine/solution.js
+++ 03. JS Advanced - Jan 2023/04. DOM Introduction/Exercise/05. Table - Search Engine/solution.js
@@ -2,7 +2,17 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      let rows = document.querySelectorAll('tbody tr');
+      let input = document.getElementById('searchField').value;
 
+      for (let row of rows) {
+
+         row.classList.remove('select'); // с това се зачистваме ако има приложение класове преди това
+
+         if (row.innerHTML.includes(input) && input !== '') {
+            row.className = 'select'; // тук добавяме на всеки ред клас, които е от ccs файла
+         }
+      }
+      input = '';
    }
 }

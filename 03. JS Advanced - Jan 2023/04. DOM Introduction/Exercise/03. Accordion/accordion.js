c95e698e042539d07985363e8b8829b86e117db5
Todor Borisov <todor0203@gmail.com>
2023-01-29 10:59:48 +0200

    Update accordion.js

--- 03. JS Advanced - Jan 2023/04. DOM Introduction/Exercise/03. Accordion/accordion.js
+++ 03. JS Advanced - Jan 2023/04. DOM Introduction/Exercise/03. Accordion/accordion.js
@@ -1,3 +1,14 @@
 function toggle() {
-    console.log('TODO:...');
+
+    let buttonElement = document.getElementsByClassName('button')[0];
+    let text = document.getElementById('extra');
+
+    if (buttonElement.textContent === 'More') {
+        buttonElement.textContent = 'Less';
+        text.style.display = 'block';
+
+    } else if (buttonElement.textContent === 'Less') {
+        buttonElement.textContent = 'More';
+        text.style.display = 'none';
+    }
 }

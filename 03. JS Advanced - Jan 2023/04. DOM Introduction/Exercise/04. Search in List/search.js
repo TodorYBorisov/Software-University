2c657c70b25cfb1087ee13e82cbfbc318879d1de
Todor Borisov <todor0203@gmail.com>
2023-01-29 11:59:21 +0200

    Update search.js

--- 03. JS Advanced - Jan 2023/04. DOM Introduction/Exercise/04. Search in List/search.js
+++ 03. JS Advanced - Jan 2023/04. DOM Introduction/Exercise/04. Search in List/search.js
@@ -1,3 +1,21 @@
 function search() {
-   // TODO
+
+   let list = document.querySelectorAll('#towns li');
+   let search = document.getElementById('searchText').value;
+
+   for (let town of list) {
+      town.style.textDecoration = '';
+      town.style.fontWeight = 'normal';
+   }
+
+   let counter = 0;
+   for (let line of list) {
+
+      if (line.textContent.includes(search)) {
+         counter++;
+         line.style.textDecoration = 'underline';
+         line.style.fontWeight = ' bold';
+      }
+   }
+   document.getElementById('result').textContent = `${counter} matches found`;
 }

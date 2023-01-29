0ced9f1b6599161f12547eb12b4752bcbc4431cc
Todor Borisov <todor0203@gmail.com>
2023-01-29 19:26:49 +0200

    Update solution.js

--- 03. JS Advanced - Jan 2023/04. DOM Introduction/Exercise/06. Format the Text/solution.js
+++ 03. JS Advanced - Jan 2023/04. DOM Introduction/Exercise/06. Format the Text/solution.js
@@ -1,3 +1,16 @@
 function solve() {
-  //TODO
+
+  let input = document.getElementById('input').value;
+  let formatedText = document.getElementById('output');
+
+  let sentences = input.split('.').filter(s => s !== '');
+  //// Изваждаме изреченията от инпута като ги делим по точка и ги филтрираме, за да нямаме празни изречения 
+
+  while (sentences.length > 0) {
+    let textParagraph = sentences.splice(0, 3).join('. ') + '.';
+
+    let p = document.createElement('p');
+    p.textContent = textParagraph;
+    formatedText.appendChild(p);
+  }
 }

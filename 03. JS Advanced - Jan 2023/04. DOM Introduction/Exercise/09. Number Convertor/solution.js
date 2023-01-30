904f97a219bc0d53b313a2cc771f1eb64ab4561a
Todor Borisov <todor0203@gmail.com>
2023-01-30 14:15:10 +0200

    Update solution.js

--- 03. JS Advanced - Jan 2023/04. DOM Introduction/Exercise/09. Number Convertor/solution.js
+++ 03. JS Advanced - Jan 2023/04. DOM Introduction/Exercise/09. Number Convertor/solution.js
@@ -1,4 +1,29 @@
 function solve() {
+    let selectMenuTo = document.getElementById('selectMenuTo');
+    let result = document.getElementById('result');
+    let input = document.getElementById('input');
 
-    //TODO...
+    let option1 = document.createElement('option');
+    option1.value = 'binary';
+    option1.innerHTML = 'Binary';
+    selectMenuTo.appendChild(option1);
+
+    let option2 = document.createElement('option');
+    option2.value = 'hexadecimal';
+    option2.innerHTML = 'Hexadecimal';
+    selectMenuTo.appendChild(option2);
+
+    document.getElementsByTagName('button')[0].addEventListener('click', whenClicked);
+
+    function whenClicked() {
+
+        if (selectMenuTo.value === 'binary') {
+
+            result.value = Number(input.value).toString(2);
+
+        } else if (selectMenuTo.value === 'hexadecimal') {
+
+            result.value = Number(input.value).toString(16).toUpperCase();
+        }
+    }
 }

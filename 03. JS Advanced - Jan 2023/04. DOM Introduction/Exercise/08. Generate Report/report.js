c36819fa11f0293709a344ed9394eefde4a738e4
Todor Borisov <todor0203@gmail.com>
2023-01-30 12:54:21 +0200

    Update report.js

--- 03. JS Advanced - Jan 2023/04. DOM Introduction/Exercise/08. Generate Report/report.js
+++ 03. JS Advanced - Jan 2023/04. DOM Introduction/Exercise/08. Generate Report/report.js
@@ -1,3 +1,23 @@
 function generateReport() {
-    //TODO
+
+    let output = document.querySelector('#output');
+    let checks = Array.from(document.querySelectorAll('thead tr th input'));
+    let tableRows = Array.from(document.querySelectorAll('tbody tr'));
+
+    let result = [];
+
+    for (let i = 0; i < tableRows.length; i++) {
+
+        let report = {};
+
+        for (let j = 0; j < checks.length; j++) {
+            if (checks[j].checked) {
+                report[checks[j].name] = tableRows[i].children[j].textContent;
+            }
+
+        }
+        result.push(report);
+    }
+
+    output.textContent = JSON.stringify(result);
 }

ddf0d57fbc268fae0b2f587a5f53fe51e3edf042
Todor Borisov <todor0203@gmail.com>
2023-02-01 10:48:03 +0200

    Update app.js

--- 03. JS Advanced - Jan 2023/05. DOM Manipulations and Events/Lab/02. Delete-From-Table/app.js
+++ 03. JS Advanced - Jan 2023/05. DOM Manipulations and Events/Lab/02. Delete-From-Table/app.js
@@ -1,3 +1,18 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    let input = document.querySelector('input');
+    let rows = document.querySelectorAll('tbody tr');
+    let resultElement = document.getElementById('result');
+
+    for (let row of rows) {
+        if (input.value === row.children[1].textContent) {
+            resultElement.textContent = 'Deleted.';
+            row.parentElement.removeChild(row);
+            if (resultElement.textContent = 'Deleted.') {
+                // input.value = '';             // Изчистваме инпут полето
+                return;
+            }
+        } else {
+            resultElement.textContent = 'Not found.';
+        }
+    }
 }

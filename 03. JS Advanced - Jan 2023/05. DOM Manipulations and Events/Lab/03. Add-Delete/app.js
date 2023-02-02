03f0b13492e4a7d20b4fb533ccabda08f5d0df71
Todor Borisov <todor0203@gmail.com>
2023-02-02 16:54:04 +0200

    Update app.js

--- 03. JS Advanced - Jan 2023/05. DOM Manipulations and Events/Lab/03. Add-Delete/app.js
+++ 03. JS Advanced - Jan 2023/05. DOM Manipulations and Events/Lab/03. Add-Delete/app.js
@@ -5,6 +5,7 @@ function addItem() {
 
     let addLiElement = document.createElement('li');
     addLiElement.textContent = input.value;
+    input.value = ''; // изчиства инпут полето
     listElement.appendChild(addLiElement);
 
     let deleteButton = document.createElement('a');

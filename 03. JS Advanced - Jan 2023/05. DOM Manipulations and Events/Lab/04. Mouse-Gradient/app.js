59ce604b8c4b54f441ce64cde8726e540a2157b5
Todor Borisov <todor0203@gmail.com>
2023-02-02 17:43:14 +0200

    Update app.js

--- 03. JS Advanced - Jan 2023/05. DOM Manipulations and Events/Lab/04. Mouse-Gradient/app.js
+++ 03. JS Advanced - Jan 2023/05. DOM Manipulations and Events/Lab/04. Mouse-Gradient/app.js
@@ -1,3 +1,11 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    let gardienPosition = document.getElementById('gradient');
+    gardienPosition.addEventListener('mousemove', onMouseMove);
+
+    let resultField = document.getElementById('result');
+
+    function onMouseMove(event) {
+        resultField.textContent = Math.floor((event.offsetX / gardienPosition.clientWidth)* 100) + '%';
+    }
+
 }

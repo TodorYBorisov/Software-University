8018fdce646027b0156380ac8b38d6561649322c
Todor Borisov <todor0203@gmail.com>
2023-02-03 10:39:31 +0200

    Update app.js

--- 03. JS Advanced - Jan 2023/05. DOM Manipulations and Events/Lab/05. Highlight-Active/app.js
+++ 03. JS Advanced - Jan 2023/05. DOM Manipulations and Events/Lab/05. Highlight-Active/app.js
@@ -1,3 +1,17 @@
 function focused() {
-    console.log('TODO:...');
+    let input = document.querySelectorAll('input');
+
+    for (const field of input) {
+
+        field.addEventListener('focus', onFocus);
+        field.addEventListener('blur', onBlur);
+    }
+
+    function onFocus(event) {
+        event.currentTarget.parentElement.classList.add('focused');
+    }
+
+    function onBlur(event) {
+        event.currentTarget.parentElement.classList.remove('focused');
+    }
 }

f6370a63e05139f753aed917d4f1a0701bfc626c
Todor Borisov <todor0203@gmail.com>
2023-02-03 14:36:49 +0200

    Update app.js

--- 03. JS Advanced - Jan 2023/05. DOM Manipulations and Events/Lab/06. Dynamic-Validation/app.js
+++ 03. JS Advanced - Jan 2023/05. DOM Manipulations and Events/Lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,18 @@
 function validate() {
-    console.log('TODO:...');
+
+    let input = document.getElementById('email');
+    
+    input.addEventListener('change', onChange);
+    
+    function onChange(event) {
+        let mailValidation = /^[a-z]+[\._-]*[a-z][\._-]*[a-z]+@[a-z]+\.[a-z]+/gm;
+
+        if(mailValidation.test(event.currentTarget.value)){
+            event.currentTarget.classList.remove('error');
+        }else{
+            event.currentTarget.classList.add('error');
+        }
+        
+    }
+
 }

18bd5dd011a8637b2bc936d5fc542a28b4cb50ab
Todor Borisov <todor0203@gmail.com>
2023-02-04 20:35:49 +0200

    Add Sections

---
+++ 03. JS Advanced - Jan 2023/05. DOM Manipulations and Events/Exercise/01. Sections/script.js
@@ -0,0 +1,22 @@
+function create(array) {
+
+   for (let section of array) {
+
+      let divElement = document.createElement('div');
+      let pElement = document.createElement('p');
+      pElement.textContent = section;
+      pElement.style.display = 'none';
+
+      let parentElement = document.getElementById('content');
+      parentElement.appendChild(divElement);
+      divElement.appendChild(pElement);
+
+      divElement.addEventListener('click', (event) => {
+
+         if (event.currentTarget.firstElementChild.style.display = 'none') {
+            event.currentTarget.firstElementChild.style.display = '';
+         }
+
+      });
+   }
+}

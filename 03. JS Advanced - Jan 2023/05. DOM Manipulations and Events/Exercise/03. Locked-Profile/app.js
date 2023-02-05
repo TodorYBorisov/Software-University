4928e3c558acc4116be424754f47d539b1157bbf
Todor Borisov <todor0203@gmail.com>
2023-02-05 16:22:25 +0200

    Update app.js

--- 03. JS Advanced - Jan 2023/05. DOM Manipulations and Events/Exercise/03. Locked-Profile/app.js
+++ 03. JS Advanced - Jan 2023/05. DOM Manipulations and Events/Exercise/03. Locked-Profile/app.js
@@ -3,23 +3,23 @@ function lockedProfile() {
     Array.from(document.querySelectorAll('.profile button')).forEach(button => {
         button.addEventListener('click', onClick);
     });
-
+  // Избираме всички бутони в дивовете с клас profile. Правим ги на масив, минаваме по всеки бутон и добавяме слушател
     function onClick(event) {
 
-        let profile = event.target.parentElement;
+        let profile = event.target.parentElement; // Намираме родителя на натиснатия бутон 
 
-        let isActive = profile.querySelector('input[value="unlock"]').checked;
+        let isActive = profile.querySelector('input[value="unlock"]').checked;// Проверяваме дали на профила радио бутона със стойност unlock е чекнат 
 
-        if (isActive) {
+        if (isActive) { // Ако е чекнат бутона 
 
-            let div = profile.querySelector('div');
+            let div = profile.querySelector('div'); // Намираме родителя на натиснатия бутон 
 
-            if (event.target.textContent === 'Show more') {
-                div.style.display = 'block';
-                event.target.textContent = 'Hide it';
-            } else {
-                div.style.display = 'none';
-                event.target.textContent === 'Show more';
+            if (event.target.textContent === 'Show more') {// Ако текста на бутона е Show More 
+                div.style.display = 'block'; // Показваме информацията
+                event.target.textContent = 'Hide it';  // Сменяме текста на бутона 
+            } else {// Ако текста на бутона е Hide it
+                div.style.display = 'none';  // Скриваме информацията
+                event.target.textContent === 'Show more'; // Сменяме текста на бутона
             }
         }
     }

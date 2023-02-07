192badbf3b778c3bd8f1f542c7da8f2719e04add
Todor Borisov <todor0203@gmail.com>
2023-02-07 17:51:14 +0200

    Update 02. Add .js

--- 03. JS Advanced - Jan 2023/06. Advanced Functions/Lab/02. Add .js
+++ 03. JS Advanced - Jan 2023/06. Advanced Functions/Lab/02. Add .js
@@ -1,7 +1,7 @@
 function add(num) {
 
-    return function sum(n) { // Създаване функция sum(която приема число) -> резултата от тази функция се връща от функцията add
-        return num + n;         // и връща сбора на n и num
+    return function sum(n) { // Създаване функция sum(която приема число)->резултата от тази функция се връща от функцията add
+        return num + n;      // и връща сбора на n и num
     };
 }
 let add5 = add(5);

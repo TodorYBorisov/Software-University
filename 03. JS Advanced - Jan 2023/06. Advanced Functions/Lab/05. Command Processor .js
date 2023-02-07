3b282e5199b3b681b08142970806f716997af762
Todor Borisov <todor0203@gmail.com>
2023-02-07 23:12:44 +0200

    Create 05. Command Processor .js

---
+++ 03. JS Advanced - Jan 2023/06. Advanced Functions/Lab/05. Command Processor .js
@@ -0,0 +1,36 @@
+function commandProcessor() {
+
+    let result = '';
+
+    function append(string) {
+        result += string;
+    };
+
+    function removeStart(n) {
+        result = result.slice(n);
+    };
+
+    function removeEnd(n) {
+        result = result.slice(0, -n);
+    }
+
+    function print() {
+        console.log(result);
+    }
+    return {
+        append,
+        removeStart,
+        removeEnd,
+        print
+    };
+}
+
+let firstZeroTest = commandProcessor();
+
+firstZeroTest.append('hello');
+firstZeroTest.append('again');
+firstZeroTest.removeStart(3);
+firstZeroTest.removeEnd(4);
+firstZeroTest.print();
+
+

215f36ff2cf9067e8ee5bbd3aad067f36b7b6b3d
Todor Borisov <todor0203@gmail.com>
2023-02-08 19:21:50 +0200

    Create 02. Argument Info .js

---
+++ 03. JS Advanced - Jan 2023/06. Advanced Functions/Exercise/02. Argument Info .js
@@ -0,0 +1,22 @@
+function argumentInfo(...input) {
+
+    let result = {};
+
+    for (let value of input) {
+        let type = typeof value;
+
+        console.log(`${type}: ${value}`);
+
+        if (!result[type]) {
+            result[type] = 0;
+        }
+        result[type] += 1;
+    }
+
+    let sort = Object.entries(result).sort((a, b) => b[1] - a[1]);
+
+    for (let [type, count] of sort) {
+        console.log(`${type} = ${count}`);
+    }
+}
+argumentInfo('cat', 42, function () { console.log('Hello world!'); });

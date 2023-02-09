108538db97f98173449f555beeb3de44a308babe
Todor Borisov <todor0203@gmail.com>
2023-02-09 23:37:41 +0200

    Create script.js

---
+++ 03. JS Advanced - Jan 2023/06. Advanced Functions/Exercise/08. Next Article/script.js
@@ -0,0 +1,14 @@
+function getArticleGenerator(articles) {
+
+    return function () {
+
+        let div = document.getElementById('content');
+
+        if (articles.length > 0) {
+
+            let articleElement = document.createElement('article');
+            articleElement.textContent = articles.shift();
+            div.appendChild(articleElement);
+        }
+    };
+};

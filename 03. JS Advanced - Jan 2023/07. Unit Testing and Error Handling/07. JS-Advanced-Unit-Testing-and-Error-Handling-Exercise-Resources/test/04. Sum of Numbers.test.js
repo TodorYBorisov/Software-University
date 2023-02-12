6d94d55ac23bf19443d6cf1b10ab6572fe0cd054
Todor Borisov <todor0203@gmail.com>
2023-02-12 22:13:24 +0200

    Update 04. Sum of Numbers.test.js

--- 03. JS Advanced - Jan 2023/07. Unit Testing and Error Handling/07. JS-Advanced-Unit-Testing-and-Error-Handling-Exercise-Resources/test/04. Sum of Numbers.test.js
+++ 03. JS Advanced - Jan 2023/07. Unit Testing and Error Handling/07. JS-Advanced-Unit-Testing-and-Error-Handling-Exercise-Resources/test/04. Sum of Numbers.test.js
@@ -7,9 +7,13 @@ describe('Check Array Sum', () => {
         expect(sum([1, 2, 3])).to.equal(6);
     });
 
-    // it('Check floating numbers', () => {
-    //     expect(sum([0.10, 0.20])).to.equal((0.3));
-    // });
+    it('Check floating numbers', () => {
+        expect(sum([0.1, 0.3])).to.equal(0.4);
+    });
+
+    it('Check two float numbers Dimo', function() {
+        expect(sum([0.1, 0.4])).to.equal(0.5);
+    });
 
     it('Check sum to be zero if array empty', () => {
         expect(sum([])).to.be.equal(0);

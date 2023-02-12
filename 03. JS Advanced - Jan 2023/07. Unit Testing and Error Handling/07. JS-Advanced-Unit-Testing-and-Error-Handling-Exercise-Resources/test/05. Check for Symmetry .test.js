4d4fb3a9ecea5f30ca5f28d0fa8a1df9aee00a14
Todor Borisov <todor0203@gmail.com>
2023-02-12 22:13:34 +0200

    Create 05. Check for Symmetry .test.js

---
+++ 03. JS Advanced - Jan 2023/07. Unit Testing and Error Handling/07. JS-Advanced-Unit-Testing-and-Error-Handling-Exercise-Resources/test/05. Check for Symmetry .test.js
@@ -0,0 +1,10 @@
+// const { expect } = require('chai');
+// const { isSymmetric } = require('../05. Check for Symmetry');
+
+// describe('', () => {
+
+//     it('', () => {
+//         expect(isSymmetric()).to.equal();
+//     });
+
+// });

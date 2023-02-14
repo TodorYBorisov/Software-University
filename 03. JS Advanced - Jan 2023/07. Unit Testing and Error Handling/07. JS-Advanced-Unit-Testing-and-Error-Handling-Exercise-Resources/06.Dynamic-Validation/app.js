b72cafe6504442e302c30cb421c3ca7d299ed5f8
Todor Borisov <todor0203@gmail.com>
2023-02-14 16:48:59 +0200

    Update app.js

--- 03. JS Advanced - Jan 2023/07. Unit Testing and Error Handling/07. JS-Advanced-Unit-Testing-and-Error-Handling-Exercise-Resources/06.Dynamic-Validation/app.js
+++ 03. JS Advanced - Jan 2023/07. Unit Testing and Error Handling/07. JS-Advanced-Unit-Testing-and-Error-Handling-Exercise-Resources/06.Dynamic-Validation/app.js
@@ -1,3 +1,14 @@
 function validate() {
-    // TODO
+    let input = document.getElementById('email');
+
+    input.addEventListener('change', () => {
+
+        let pattern = /[a-z]+@[a-z]+[.][a-z]+/gm;
+       
+        if (pattern.test(input.value)) {
+            input.classList.remove('error');
+        } else {
+            input.classList.add('error');
+        }
+    });
 }

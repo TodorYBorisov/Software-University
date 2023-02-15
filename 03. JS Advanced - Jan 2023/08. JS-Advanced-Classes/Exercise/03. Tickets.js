d3a45bfffe1f4584c6446c14e69d045a1f356a6b
Todor Borisov <todor0203@gmail.com>
2023-02-15 15:51:31 +0200

    Create 03. Tickets.js

---
+++ 03. JS Advanced - Jan 2023/08. JS-Advanced-Classes/Exercise/03. Tickets.js
@@ -0,0 +1,37 @@
+function tickets(array, string) {
+
+    class Ticket {
+        constructor(destination, price, status) {
+            this.destination = destination;
+            this.price = Number(price);
+            this.status = status;
+        }
+    }
+
+    let result = [];
+
+    for (let line of array) {
+
+        let [destination, price, status] = line.split('|');
+        let ticket = new Ticket(destination, price, status);
+
+        result.push(ticket);
+    }
+
+    if (string === 'destination') {
+        result = result.sort((a, b) => a[string].localeCompare(b[string]));
+    } else if (string === 'price') {
+        result = result.sort((a, b) => a[string] - b[string]);
+    } else if (string === 'status') {
+        result = result.sort((a, b) => a[string].localeCompare(b[string]));
+    }
+
+    console.log(result);
+
+};
+tickets(['Philadelphia|94.20|available',
+    'New York City|95.99|available',
+    'New York City|95.99|sold',
+    'Boston|126.20|departed'],
+    'destination'
+);

4f9cb6d8bfb643370f5f9875d48a881a414fde27
Todor Borisov <todor0203@gmail.com>
2023-03-02 15:38:26 +0200

    Update app.js

--- 04. JS Applications/02 Asynchronous Programming/Lab/01.XHR/scripts/app.js
+++ 04. JS Applications/02 Asynchronous Programming/Lab/01.XHR/scripts/app.js
@@ -1,3 +1,16 @@
+let button = document.querySelector('#load');
+button.addEventListener('click', loadRepos);
+
 function loadRepos() {
-   console.log("TODO...");
-}
+
+   let url = 'https://api.github.com/users/testnakov/repos';
+   const httpRequest = new XMLHttpRequest();
+   
+   httpRequest.addEventListener('readystatechange', function () {
+      if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+         document.getElementById('res').textContent = httpRequest.responseText;
+      }
+   });
+   httpRequest.open('GET', url);
+   httpRequest.send();
+};

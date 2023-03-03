ded1296b5c3766cc522eb0fbdad05d921e94d11f
Todor Borisov <todor0203@gmail.com>
2023-03-03 12:41:59 +0200

    Update demo.js

--- 04. JS Applications/02 Asynchronous Programming/Lab/demo.js
+++ 04. JS Applications/02 Asynchronous Programming/Lab/demo.js
@@ -2,4 +2,13 @@ console.log('Hello.');
 
 setTimeout(() => { console.log('Goodbye!'); }, 2000);
 
-console.log('Hello again!'); 
+console.log('Hello again!');
+
+//callback function
+function running() {
+    return 'Running';
+}
+function category(run, type) {
+    console.log(run() + ' ' + type);
+}
+category(running, 'sprint');

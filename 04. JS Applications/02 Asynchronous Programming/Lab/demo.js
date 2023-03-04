89796eaf633a9a4daebb5c866a4ccf7f880f07b1
Todor Borisov <todor0203@gmail.com>
2023-03-04 19:47:59 +0200

    Update demo.js

--- 04. JS Applications/02 Asynchronous Programming/Lab/demo.js
+++ 04. JS Applications/02 Asynchronous Programming/Lab/demo.js
@@ -1,49 +1,66 @@
-console.log('Hello.');
+// console.log('Hello.');
 
-setTimeout(() => { console.log('Goodbye!'); }, 2000);
+// setTimeout(() => { console.log('Goodbye!'); }, 2000);
 
-console.log('Hello again!');
+// console.log('Hello again!');
 
-//callback function
+// //callback function
 
-function running() {
-    return 'Running';
+// function running() {
+//     return 'Running';
+// }
+// function category(run, type) {
+//     console.log(run() + ' ' + type);
+// }
+// category(running, 'sprint');
+
+// ///////////////////////////////////////////////////
+
+// function executor(resolve, reject) {
+//     console.log('promise starting');
+
+//     // setTimeout(() => {
+//     //     reject(new Error('Simulated error'));
+//     // }, 1000);
+
+//     setTimeout(() => {
+//         resolve('hello');
+//     }, 2000);
+
+//     console.log('promise ended');
+
+// }
+// let promise = new Promise(executor);
+// promise.then(successCallback);
+// promise.catch(failureCallback);
+// promise.finally(() => {
+//     console.log('operation completed');
+// });
+
+// function successCallback(data) {
+//     console.log('received data:', data);
+// }
+
+// function failureCallback(error) {
+//     console.log('Encountered error', error.message);
+// }
+//asyn/await
+
+function createTimer() {
+    return new Promise(resolve => {
+        setTimeout(() => resolve('hello'), 2000);
+    });
 }
-function category(run, type) {
-    console.log(run() + ' ' + type);
+async function start() {
+    const timerPromise = createTimer();
+
+    console.log('starting');
+
+    const data = await timerPromise;
+
+    console.log('operation complete:', data);
+
+    console.log('end of scope');
 }
-category(running, 'sprint');
-
-///////////////////////////////////////////////////
-
-function executor(resolve, reject) {
-    console.log('promise starting');
-
-    // setTimeout(() => {
-    //     reject(new Error('Simulated error'));
-    // }, 1000);
-
-    setTimeout(() => {
-        resolve('hello');
-    }, 2000);
-
-    console.log('promise ended');
-
-}
-let promise = new Promise(executor);
-promise.then(successCallback);
-promise.catch(failureCallback);
-promise.finally(() => {
-    console.log('operation completed');
-});
-
-function successCallback(data) {
-    console.log('received data:', data);
-}
-
-function failureCallback(error) {
-    console.log('Encountered error', error.message);
-}
-//fetch
-
+start();
 

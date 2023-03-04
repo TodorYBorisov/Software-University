9f96e488a3e6cff8fce0fe38cf68f6e8128dcc1c
Todor Borisov <todor0203@gmail.com>
2023-03-04 22:48:56 +0200

    Create app1.js

---
+++ 04. JS Applications/02 Asynchronous Programming/Lab/03.Gighub-Commits/app1.js
@@ -0,0 +1,33 @@
+async function loadCommits() {
+
+    let username = document.getElementById('username').value;
+    let repository = document.getElementById('repo').value;
+    let output = document.getElementById('commits');
+    output.innerHTML = '';
+    
+    const url = `https://api.github.com/repos/${username}/${repository}/commits`;
+    try {
+        
+        let response = await fetch(url);
+        
+        if (response.ok === false) {
+            throw new Error(`Error: ${response.status} (Not Found)`);
+        }
+        
+        let data = await response.json();
+        
+
+        for (let entry of data) {
+
+            let li = document.createElement('li');
+            li.textContent = `${entry.commit.author.name}: ${entry.commit.message}`;
+            output.appendChild(li);
+        }
+
+    } catch (error) {
+        let li = document.createElement('li');
+        li.textContent = `${error.message}`;
+        output.appendChild(li);
+    }
+
+}

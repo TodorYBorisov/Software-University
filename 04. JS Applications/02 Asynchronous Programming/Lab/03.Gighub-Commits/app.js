2c04a9c4235bb678223951541b3756f4477d8c9b
Todor Borisov <todor0203@gmail.com>
2023-03-04 22:36:31 +0200

    Update app.js

--- 04. JS Applications/02 Asynchronous Programming/Lab/03.Gighub-Commits/app.js
+++ 04. JS Applications/02 Asynchronous Programming/Lab/03.Gighub-Commits/app.js
@@ -1,4 +1,33 @@
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
-}
+
+    let username = document.getElementById('username').value;
+    let repository = document.getElementById('repo').value;
+    let output = document.getElementById('commits');
+    output.innerHTML = '';
+
+    const url = `https://api.github.com/repos/${username}/${repository}/commits`;
+
+
+    fetch(url).then((response) => {
+
+        if (response.ok === false) {
+            throw new Error(`Error: ${response.status} (Not Found)`);
+        }
+        return response.json();
+
+
+    }).then((data) => {
+
+        for (let entry of data) {
+
+            let li = document.createElement('li');
+            li.textContent = `${entry.commit.author.name}: ${entry.commit.message}`;
+            output.appendChild(li);
+        }
+
+    }).catch((error) => {
+        let li = document.createElement('li');
+        li.textContent = `${error.message}`;
+        output.appendChild(li);
+    });
+};

dd5b6e88c36818761557686409422b870219a9cd
Todor Borisov <todor0203@gmail.com>
2023-03-05 19:31:14 +0200

    Update app.js

--- 04. JS Applications/02 Asynchronous Programming/Exercise/01.Bus-Stop/app.js
+++ 04. JS Applications/02 Asynchronous Programming/Exercise/01.Bus-Stop/app.js
@@ -1,3 +1,33 @@
-function getInfo() {
-    console.log("TODO...");
-}
+async function getInfo() {
+
+    let stopId = document.getElementById('stopId').value;
+    let list = document.getElementById('buses');
+    let stopName = document.getElementById('stopName');
+
+    let url = `http://localhost:3030/jsonstore/bus/businfo/${stopId}`;
+
+    try {
+        let response = await fetch(url);
+
+        if (response.ok === false) {
+            throw new Error(`${response.status}: ${response.statusText}`);
+        }
+
+        let data = await response.json();
+
+        let name = data.name;
+        let busesObj = data.buses;
+
+        stopName.textContent = name;
+        list.innerHTML ='';
+        for (let bus in busesObj ) {
+
+            let li = document.createElement('li');
+            li.textContent = `Bus ${bus} arrives in ${busesObj[bus]} minutes`;
+            list.appendChild(li);
+        }
+    } catch (error) {
+        stopName.textContent = 'Error';
+        list.innerHTML ='';
+    }
+};

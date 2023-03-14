6c1e30a4c2858848dc7b0b5e7ba899dd63042445
Todor Borisov <todor0203@gmail.com>
2023-03-14 17:59:36 +0200

    Update app.js

--- 04. JS Applications/03 Remote Data and Authentication/Exercise/01.Messenger/app.js
+++ 04. JS Applications/03 Remote Data and Authentication/Exercise/01.Messenger/app.js
@@ -1,5 +1,72 @@
 function attachEvents() {
-    console.log('TODO...');
-}
 
+    const nameAuthor = document.querySelector('input[name="author"]');
+    const contentAuthor = document.querySelector('input[name="content"]');
+
+    const sendBtn = document.getElementById('submit');
+    sendBtn.addEventListener('click', submitData);
+
+    async function submitData() {
+       
+        if (nameAuthor.value == '' || contentAuthor.value == '') {
+            return;
+        }
+
+        let newObj = {
+            author: nameAuthor.value,
+            content: contentAuthor.value
+        };
+
+        const url = 'http://localhost:3030/jsonstore/messenger';
+        const options = {
+            method: 'post',
+            headers: { 'Content-type': 'applications/json' },
+            body: JSON.stringify(newObj)
+        };
+        try {
+            const response = await fetch(url, options);
+            
+            if (response.ok == false) {
+                throw new Error('Error');
+            }
+            const data = await response.json();
+
+            return data;
+            
+        } catch (error) {
+            alert(error.message);
+            throw error;
+        }  
+    }
+
+    let refreshBtn = document.getElementById('refresh');
+    refreshBtn.addEventListener('click', loadData);
+
+    async function loadData() {
+
+        const url = 'http://localhost:3030/jsonstore/messenger';
+        try {
+            const response = await fetch(url);
+            if (response.ok === false) {
+                const error = await response.json();
+                throw error;
+            }
+
+            const data = await response.json();
+            const arr = Object.values(data);
+
+            const area = document.getElementById('messages');
+
+            let allComments = [];
+            for (let line of arr) {
+                allComments.push(`${line.author}: ${line.content}`);
+                area.value = allComments.join('\n');
+            }
+
+        } catch (error) {
+            alert(error.message);
+        }
+       
+    }
+}
 attachEvents();

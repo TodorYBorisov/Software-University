6bae3840898d071bda1341d3995a64c558448c5a
Todor Borisov <todor0203@gmail.com>
2023-03-16 18:00:13 +0200

    Create helping functions.js

---
+++ 04. JS Applications/03 Remote Data and Authentication/Lab/helping functions.js
@@ -0,0 +1,44 @@
+function createElement(type, attr, ...content) {
+
+    const element = document.createElement(type);
+
+    for (const item in attr) {
+        if (item === 'class') {
+            element.classList.add(attr[item]);
+        } else if (item === 'disable') {
+            element.disabled = attr[item];
+        } else {
+            element[item] = attr[item];
+        }
+    }
+
+    for (let item of content) {
+        if (typeof item === 'string' || typeof item === 'number') {
+            item = document.createTextNode(item);
+        }
+        element.appendChild(item);
+    }
+    return element;
+}
+
+function createElement2(typeEl, content, parentEl, atributes) {
+    const element = document.createElement(typeEl);
+    element.textContent = content;
+
+    if (atributes) {
+        for (let atrr in atributes) {
+            element.setAttribute(atrr, atributes[atrr]);
+        }
+    }
+    parentEl.appendChild(element);
+    return element;
+}
+
+function getHeaders(method, options) {
+
+    return {
+        method: `${method}`,
+        headers: { 'Content-type': 'application/json' },
+        body: JSON.stringify(options)
+    };
+}

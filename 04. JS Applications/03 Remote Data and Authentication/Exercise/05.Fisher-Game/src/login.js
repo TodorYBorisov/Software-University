23bf6a2d06644d1c66a428ff4b9284e907d2d0ef
Todor Borisov <todor0203@gmail.com>
2023-03-16 18:00:22 +0200

    Update login.js

--- 04. JS Applications/03 Remote Data and Authentication/Exercise/05.Fisher-Game/src/login.js
+++ 04. JS Applications/03 Remote Data and Authentication/Exercise/05.Fisher-Game/src/login.js
@@ -1,1 +1,46 @@
-console.log('TODO:// Implement Login functionality');
+document.getElementById('user').style.display = 'none';
+
+document.getElementById('login-form').addEventListener('submit', onLogin);
+let notification = document.querySelector('.notification');
+
+async function onLogin(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+
+    const { email, password } = Object.fromEntries(formData.entries());
+
+    if (email === '' || password === '') {
+        notification.textContent = 'All fileds must be filled!';
+        return alert('No empy fields allowed!');
+    };
+
+    const url = 'http://localhost:3030/users/login';
+
+    const options = {
+        method: 'post',
+        headers: { 'Content-type': 'applications/json' },
+        body: JSON.stringify({ email, password })
+    };
+    try {
+        const response = await fetch(url, options);
+        if (response.ok == false) {
+            const error = await response.json();
+            throw error;
+        }
+
+        const userData = await response.json();
+        
+        localStorage.setItem('email', userData.email);
+        localStorage.setItem('accessToken', userData.accessToken);
+        localStorage.setItem('_id', userData._id);
+
+        event.target.reset();
+
+        window.location = ('./index.html');
+
+    } catch (error) {
+        document.getElementById('login-form').reset();
+        alert(error.message);
+    }
+}

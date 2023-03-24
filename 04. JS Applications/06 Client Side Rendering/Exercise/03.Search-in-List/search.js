4612cc7ff6e564ddfec702056d23a9978f789aca
Todor Borisov <todor0203@gmail.com>
2023-03-24 13:47:59 +0200

    Update search.js

--- 04. JS Applications/06 Client Side Rendering/Exercise/03.Search-in-List/search.js
+++ 04. JS Applications/06 Client Side Rendering/Exercise/03.Search-in-List/search.js
@@ -1,3 +1,26 @@
+import { towns } from './towns.js';
+import { html, render } from '../../node_modules/lit-html/lit-html.js';
+
+const mainDiv = document.getElementById('towns');
+
+const townTepmlate = html`<ul> ${towns.map((town) => html`<li id=${town}>${town}</li>`)}</ul>`;
+
+render(townTepmlate, mainDiv);
+
+const btn = document.querySelector('button').addEventListener('click', search);
+
 function search() {
-   // TODO
+
+   let text = document.getElementById('searchText').value;
+   let result = towns.filter((town) => {
+      if (town.includes(text)) {
+         let match = document.getElementById(`${town}`);
+         match.setAttribute('class', 'active');
+         return town;
+      }
+
+   });
+
+let resultHTML = document.getElementById('result');
+resultHTML.textContent = `${result.length} matches found`;
 }

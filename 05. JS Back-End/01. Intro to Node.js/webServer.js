3b5d913724a4050975a9ea026d8a25a8377f9929
Todor Borisov <todor0203@gmail.com>
2023-04-11 17:31:30 +0300

    Update webServer.js

---
+++ 05. JS Back-End/01. Intro to Node.js/webServer.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    console.log('HHTP Request');
+    res.write('Hello from NodeJS!');
+    res.end();
+});
+  
+server.listen(5000);
+
+console.log('Server is running on port 5000...'); 

f0bdf324dbe2eaddc106e35020da61ffe18b2aa4
Todor Borisov <todor0203@gmail.com>
2023-04-24 16:24:21 +0300

    Update webServer.js

--- 05. JS Back-End/01. Intro to Node.js/webServer.js
+++ 05. JS Back-End/01. Intro to Node.js/webServer.js
@@ -1,11 +1,28 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log('HHTP Request');
-    //console.log(req.headers);
-    //console.log(req.method);
-    console.log(req.url);
-    res.write('Hello from NodeJS!');
+    //console.log('HHTP Request');
+    //console.log(req.headers); какви са хедърите
+    //console.log(req.method); какъв е метода
+    //console.log(req.url);  може да видим какъв е url
+
+    res.writeHead(200, {
+        'content-type': 'text/html'
+    });
+
+    switch (req.url) {
+        case '/':
+            res.write('<h1>Hello from Node JS!</h1>');
+            break;
+        case '/cats':
+            res.write('Some Cats here');
+            break;
+
+        default:
+            res.write('Anything else');
+            break;
+    }
+
     res.end();
 });
 

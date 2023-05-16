cda44741574ef3de230fa0a8574811a9ffe6f705
Todor Borisov <todor0203@gmail.com>
2023-05-16 22:23:40 +0300

    Update url.js

--- 05. JS Back-End/01. Intro to Node.js/url.js
+++ 05. JS Back-End/01. Intro to Node.js/url.js
@@ -4,4 +4,4 @@ const myURL = new URL('https://user:pass@sub.example.com:8080/p/a/t/h?query=stri
 const soft = new URL('https://softuni.bg/search/results?SearchTerm=JS+Back-End+-+%D1%8F%D0%BD%D1%83%D0%B0%D1%80%D0%B8+2023&FastTrackAndCourseInstances=true&Seminars=true&Users=true&LectureResources=true');
 
 
-console.log(soft);
+console.log(soft)

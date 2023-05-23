39da0e91df1d54ebd9492d7798f49aaa4a067d20
Todor Borisov <todor0203@gmail.com>
2023-05-23 18:56:11 +0300

    Update webServer1.js

--- 05. JS Back-End/01. Intro to Node.js/webServer1.js
+++ 05. JS Back-End/01. Intro to Node.js/webServer1.js
@@ -1,6 +1,11 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+
+    console.log(req.headers);
+    console.log(req.method);
+    console.log(req.url);
+
     res.write('Hello from Node JS server!');
     res.end();
 });

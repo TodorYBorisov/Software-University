7faef9bcac826d95cf66fc2ee49fa1c28a4a530f
Todor Borisov <todor0203@gmail.com>
2023-05-23 18:37:40 +0300

    Create webServer1.js

---
+++ 05. JS Back-End/01. Intro to Node.js/webServer1.js
@@ -0,0 +1,31 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    //console.log('HHTP Request');
+    //console.log(req.headers); какви са хедърите
+    //console.log(req.method); какъв е метода
+    //console.log(req.url);  може да видим какъв е url
+
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+
+    switch (req.url) {
+        case '/':
+            res.write('<h1>Hello from Node JS!</h1>');
+            break;
+        case '/cats':
+            res.write('Some Cats here');
+            break;
+
+        default:
+            res.write('Anything else');
+            break;
+    }
+
+    res.end();
+});
+
+server.listen(5000);
+
+console.log('Server is running on port 5000...');

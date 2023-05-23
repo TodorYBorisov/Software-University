12ef3c2eb7e3a12cbe34ccbbddcb4e60428cbc6f
Todor Borisov <todor0203@gmail.com>
2023-05-23 20:43:55 +0300

    Update index.js

--- 05. JS Back-End/01. Intro to Node.js/01. Cat-Shelter-Workshop-Resources/index.js
+++ 05. JS Back-End/01. Intro to Node.js/01. Cat-Shelter-Workshop-Resources/index.js
@@ -1,6 +1,31 @@
 const http = require('http');
+const homeHTML = require('./resources/views/home/index');
+const siteCss = require('./resources/content/styles/site');
+const addBreedHTML = require('./resources/views/addBreed');
+const addCatHTML = require('./resources/views/addCat');
 
 const server = http.createServer((req, res) => {
+    const url = req.url; // взимаме url
+    console.log(url);
+
+    if (url == '/') {
+        res.writeHead(200, { 'Content-type': 'text/html' });
+        res.write(homeHTML);
+
+    } else if (url == '/content/styles/site.css') {
+        res.writeHead(200, { 'Content-type': 'text/css' });
+        res.write(siteCss);
+    } else if (url == '/cats/add-breed') {
+        res.writeHead(200, { 'Content-type': 'text/html' });
+        res.write(addBreedHTML);
+    } else if (url == '/cats/add-cats') {
+        res.writeHead(200, { 'Content-type': 'text/html' });
+        res.write(addCatHTML);
+    }
+
+    res.end();
+
+
 
 });
 server.listen(5000);

803d66bc3e0bb68afe11eccac0d56aeb06b81dba
Todor Borisov <todor0203@gmail.com>
2023-05-25 11:48:51 +0300

    Update index.js

--- 05. JS Back-End/01. Intro to Node.js/01. Cat-Shelter-Workshop-Resources/index.js
+++ 05. JS Back-End/01. Intro to Node.js/01. Cat-Shelter-Workshop-Resources/index.js
@@ -1,23 +1,51 @@
 const http = require('http');
-const homeHTML = require('./resources/views/home/index');
+const { homeTemplate, catTemplate } = require('./resources/views/home/index');
 const siteCss = require('./resources/content/styles/site');
 const addBreedHTML = require('./resources/views/addBreed');
 const addCatHTML = require('./resources/views/addCat');
 
-const server = http.createServer((req, res) => {
+
+const cats = [
+    {
+        id: '1',
+        name: 'Cat1',
+        breed: 'Breed1',
+        description: 'Description for cat 1'
+    },
+    {
+        id: '2',
+        name: 'Cat2',
+        breed: 'Breed2',
+        description: 'Description for cat 2'
+    },
+    {
+        id: '3',
+        name: 'Cat3',
+        breed: 'Breed3',
+        description: 'Description for cat 3'
+    }
+];
+
+const server = http.createServer(async (req, res) => {
     const url = req.url; // взимаме url
     console.log(url);
 
     if (url == '/') {
+
+        const catsHTML = cats.map(cat => catTemplate.replace('{{name}}', cat.name));
+        const homeHTML = homeTemplate.replace('{{cats}}', catsHTML);
+
         res.writeHead(200, { 'Content-type': 'text/html' });
         res.write(homeHTML);
 
     } else if (url == '/content/styles/site.css') {
         res.writeHead(200, { 'Content-type': 'text/css' });
         res.write(siteCss);
+
     } else if (url == '/cats/add-breed') {
         res.writeHead(200, { 'Content-type': 'text/html' });
         res.write(addBreedHTML);
+
     } else if (url == '/cats/add-cats') {
         res.writeHead(200, { 'Content-type': 'text/html' });
         res.write(addCatHTML);

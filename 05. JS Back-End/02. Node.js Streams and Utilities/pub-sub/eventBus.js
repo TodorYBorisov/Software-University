ff05a75eef164cbdd4f7fa861392fdba22599f23
Todor Borisov <todor0203@gmail.com>
2023-05-25 14:01:32 +0300

    Update eventBus.js

---
+++ 05. JS Back-End/02. Node.js Streams and Utilities/pub-sub/eventBus.js
@@ -0,0 +1,22 @@
+const listeners = {};
+
+const publish = (eventName) => {
+    
+    listeners[eventName].forEach(listener => listener());
+};
+
+const subscribe = (eventName, eventListener) => {
+    if (!listeners[eventName]) {
+        listeners[eventName] = [];
+    }
+
+    listeners[eventName].push(eventListener);
+
+};
+
+const eventBus = {
+    publish,
+    subscribe,
+};
+
+module.exports = eventBus;

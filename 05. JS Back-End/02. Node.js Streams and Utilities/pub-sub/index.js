d7525ada88c0812739e78afaface8575831ca87b
Todor Borisov <todor0203@gmail.com>
2023-05-25 14:39:40 +0300

    Update index.js

--- 05. JS Back-End/02. Node.js Streams and Utilities/pub-sub/index.js
+++ 05. JS Back-End/02. Node.js Streams and Utilities/pub-sub/index.js
@@ -1,12 +1,20 @@
 const eventBus = require('./eventBus');
 
-eventBus.subscribe('user-added', () => {
-    console.log('New user is added 1!');
+// eventBus.subscribe('user-added', (userName, age) => {
+//     console.log(`New user is added - ${userName} who is ${age} years old.`);
 
-});
+// });
 eventBus.subscribe('user-added', () => {
     console.log('New user is added 2!');
 
 });
 
-eventBus.publish('user-added');
+const unsubscribe = eventBus.subscribe('user-added', (userName, age) => {
+    console.log(`New user is added - ${userName} who is ${age} years old.`);
+
+});
+
+eventBus.publish('user-added', 'Pesho', 20);
+
+unsubscribe();
+eventBus.publish('user-added', 'Pesho', 20);

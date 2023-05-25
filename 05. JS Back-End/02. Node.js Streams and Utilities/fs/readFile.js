7ff1de406a213b2570475d462b6836a211eb2321
Todor Borisov <todor0203@gmail.com>
2023-05-25 16:38:04 +0300

    Create readFile.js

---
+++ 05. JS Back-End/02. Node.js Streams and Utilities/fs/readFile.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+fs.readFile('./input.txt', 'utf-8', (err, text) => {
+
+    if (err) {
+        console.log(err);
+        return;
+    }
+
+    console.log(text);
+});
+
+console.log('End');
+

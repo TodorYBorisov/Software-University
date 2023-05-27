6546f03b4e98e016530f71c41210945c3ba67034
Todor Borisov <todor0203@gmail.com>
2023-05-27 13:52:31 +0300

    Update index.js

--- 05. JS Back-End/03. ExpressJS and Templating/Demo/index.js
+++ 05. JS Back-End/03. ExpressJS and Templating/Demo/index.js
@@ -1,31 +1,46 @@
 const express = require('express');
 const app = express(); // правим си инстанция 
+const path = require('path');
 
 
-//add middleware
+//тук добавяме един middleware
 
 app.use((req, res, next) => {
-    console.log(`HTTP request${req.method}:${req.path}`);
+    console.log(`HTTP request ${req.method}: ${req.path}`);
     next();
 });
 
-//express router/ Actions
+//custom midleware
+
+app.use('/user/:userID', (req, res, next) => {
+
+    let userExist = true;
+    if (!userExist) {
+        res.redirect('/login');
+    } else {
+        next();
+    }
+
+    app.get('/user/userId', (req, res) => {
+        res.send('User home page!');
+    });
+});
+
+
+//=====================================================================//
+//От тука надолу е нашия Express router/Actions
 
 //може да си слагаме статус, който да се чейнва
 app.get('/', (req, res) => {
     res.status(200).res.send('Welcome to Express!');
 });
 
+
 //подаваме вида ня заявката, след което указваме и пътя и в комбинация с хендлъра се нарича=> ендпоинт
 //Action e самия рикуест хендлър
 app.get('/cats', (req, res) => {
     res.status(200);
     res.send('This page contains cats');
-});
-
-app.get('/cats/:catsId', (req, res) => {
-
-    res.send(`Request with parameter - ${req.params.catsId}`);
 });
 
 app.get('old-route', (req, res) => {
@@ -45,6 +60,39 @@ app.delete('/cats', (req, res) => {
     res.status(200);
     res.send('This data has been deleted!');
 });
+
+//тук catsId е променлива/параметър идват като обект
+app.get('/cats/:catId', (req, res) => {
+
+    const catId = Number(req.params.catId);
+    if (!catId) {
+        //задължително му слагане return, за да прекъсне!!!
+        return res.status(404).send('Can find resource!');
+    }
+
+    const paramsObj = req.params;
+    console.log(paramsObj);
+
+    res.send(`Request with parameter - ${paramsObj.catId}`);
+});
+
+app.get('/download', (req, res) => {
+
+    res.download('./test.pdf');
+
+    // res.attachment('./test.pdf');
+    // res.end();
+
+    //това зарежда файла директно в браузъра направо, но иска path библиотеката
+    //  res.sendFile(path.resolve(__dirname, 'test.pdf'));
+
+});
+
+//редиректва от стар път към нов такъв
+app.get('/old-route', (req, res) => {
+    res.redirect('/cats');
+});
+
 
 //реда има значение и заради това това се слага най-отдолу и се ползва за някакви общи страници за който не е описан път
 //* да отговаря на какво и да било

0307d7f97dd2ff6dc71993b28bb466aad855e5f6
Todor Borisov <todor0203@gmail.com>
2023-05-28 13:22:41 +0300

    Update index.js

--- 05. JS Back-End/03.1 Workshop ExpressJS and Templating/03. Cubicle-Workshop-Part-1-Todor Borisov/src/index.js
+++ 05. JS Back-End/03.1 Workshop ExpressJS and Templating/03. Cubicle-Workshop-Part-1-Todor Borisov/src/index.js
@@ -1,1 +1,18 @@
-console.log('Toshko');
+const express = require('express');
+const app = express();
+const port = 5000;
+
+const handlebars = require('express-handlebars');
+//настройка на хендълбарс
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}));
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+
+    res.send('Test');
+
+});
+
+app.listen(port, () => console.log(`Server is listening on port ${port}...`));

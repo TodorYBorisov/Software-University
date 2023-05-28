fea89a91910fa3cfe581301f42548488176a0a6b
Todor Borisov <todor0203@gmail.com>
2023-05-28 13:24:17 +0300

    Add express and express-handlebars to project with set up for hbs.

--- 05. JS Back-End/03.1 Workshop ExpressJS and Templating/03. Cubicle-Workshop-Part-1-Todor Borisov/src/index.js
+++ 05. JS Back-End/03.1 Workshop ExpressJS and Templating/03. Cubicle-Workshop-Part-1-Todor Borisov/src/index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 5000;
 
 const handlebars = require('express-handlebars');
+
 //настройка на хендълбарс
 app.engine('hbs', handlebars.engine({
     extname: 'hbs'
@@ -10,9 +11,7 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
-
     res.send('Test');
-
 });
 
 app.listen(port, () => console.log(`Server is listening on port ${port}...`));

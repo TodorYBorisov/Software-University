ea8abd9bb5f6866faa1542dbae48228d7e885471
Todor Borisov <todor0203@gmail.com>
2023-05-31 00:22:05 +0300

    Update index.js

---
+++ 05. JS Back-End/04. NoSQL and MongoDB/Demo/index.js
@@ -0,0 +1,14 @@
+const mongodb = require('mongodb');
+
+const MongoClient = mongodb.MongoClient;
+const connectionStr = 'mongodb://localhost:27017';
+const client = new MongoClient(connectionStr, { useUnifiedTopology: true });
+client.connect(function (err) {
+    const db = client.db('testdb');
+    const people = db.collection('people');
+    people.insertOne({ 'name': 'Ivan' }, (err, result) => {
+        people.find({ name: 'Ivan' }).toArray((err, data) => {
+            console.log(data);
+        });
+    });
+});

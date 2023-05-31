67950e4d79d641de7e96512f23f3f88533144dfa
Todor Borisov <todor0203@gmail.com>
2023-05-31 09:59:37 +0300

    Add virtual property and some validations methods

--- 05. JS Back-End/04. NoSQL and MongoDB/mongooseDemo/index.js
+++ 05. JS Back-End/04. NoSQL and MongoDB/mongooseDemo/index.js
@@ -1,9 +1,19 @@
 const mongoose = require('mongoose');
 
+const Cat = require('./models/Cat');
+
 async function conectDB() {
 
-await mongoose.connect('mongodb://127.0.0.1:27017/mongoDB');
+    await mongoose.connect('mongodb://127.0.0.1:27017/mongoDB');
 
-console.log('DB is connected');
+    console.log('DB is connected');
+
+    const cats = await Cat.find();
+
+    //cats.forEach(cat => cat.greet());
+
+    //така вадим виртуалното пропърти
+    cats.forEach(cat =>console.log(cat.info));
+
 }
 conectDB();
--- 05. JS Back-End/04. NoSQL and MongoDB/mongooseDemo/models/Cat.js
+++ 05. JS Back-End/04. NoSQL and MongoDB/mongooseDemo/models/Cat.js
@@ -1,7 +1,32 @@
 const mongoose = require('mongoose');
 
 const catSchema = new mongoose.Schema({
-    name: String,
+    name: {
+        type: String,
+        required: true,
+        minLength: 2,// добавяме вграден валидатор
+        maxLength: 10
+    }, // можем да задаваме специфично пропърти и по този начин гарантираме, че ще го има
     age: Number,
     breed: String,
 });
+
+//може да слагме методи на схемата, къдeто this сочи към конкретния докуемт един вид
+catSchema.methods.greet = function () {
+    console.log(`Hello i\'m cat with name ${this.name}`);
+};
+
+//създаване на виртуално пропърти
+catSchema.virtual('info').get(function () {
+    return `My name is ${this.name} nad I'm ${this.age} years old.`;
+});
+
+//валидиане на данни през схемата, стринга ни е error message
+catSchema.path('name').validate(function () {
+    return this.name.length >= 2 && this.name.length <= 10;
+
+}, 'The name must be between 2 and 10 symbols long!');
+
+const Cat = mongoose.model('Cat', catSchema);
+
+module.exports = Cat;

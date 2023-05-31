40501d582da8b252c7f8ac3034ff9c46460028c6
Todor Borisov <todor0203@gmail.com>
2023-05-31 11:33:10 +0300

    Update index.js

--- 05. JS Back-End/04. NoSQL and MongoDB/mongooseDemo/index.js
+++ 05. JS Back-End/04. NoSQL and MongoDB/mongooseDemo/index.js
@@ -18,9 +18,9 @@ async function conectDB() {
     // const result = await Cat.giveMeCats();
 
     //READ from DB
-    //const cats = await Cat.find({age:5});
-    //const cats = await Cat.findOne();
-    //const cats = await Cat.findById('6476f83eb5c1fa3eaf69f2ca');
+    // const cats = await Cat.find({age:5});
+    // const cats = await Cat.findOne();
+    const cats = await Cat.findById('6476f83eb5c1fa3eaf69f2ca');
 
     //CREATE and Save to  DB
     // const newCat = new Cat({
@@ -31,26 +31,32 @@ async function conectDB() {
     // await newCat.save();
 
 
-    // const newCat = await Cat.create({
-    //     name: 'Toshko',
-    //     age: 12,
-    //     breed: 'Angora'
-    // });
+    const newCat = await Cat.create({
+        name: 'Toshko',
+        age: 12,
+        breed: 'Angora'
+    });
 
-    //UDATE DB
+   // UDATE DB
     // const toshko = await Cat.findOne({name:'Toshko'});
     // toshko.age = 39;
     // await toshko.save();
 
-    //await Cat.findByIdAndUpdate('6476fd11f201d486893cec30', { $set: { age: 15 } });
+    await Cat.findByIdAndUpdate('6476fd11f201d486893cec30', { $set: { age: 15 } });
 
     //DELETE from DB
-    //await Cat.deleteOne({name: 'Pesho'});
+    await Cat.deleteOne({name: 'Pesho'});
 
-    //await Cat.findByIdAndDelete('6476fd11f201d486893cec30');
+    await Cat.findByIdAndDelete('6476fd11f201d486893cec30');
 
 
-    console.log();
+    
+    const newOwner = await Owner.create({
+        name: 'Toshko',
+        age: 39,
+        
+    });
+    console.log(newOwner);
 
 }
 conectDB();

b82ff438487fa4612410e449329e3ffa2315f1b4
Todor Borisov <todor0203@gmail.com>
2023-06-02 15:22:31 +0300

    Add router to accessories

--- 05. JS Back-End/04.1 Workshop MongoDB and Mongoose/src/config/routes.js
+++ 05. JS Back-End/04.1 Workshop MongoDB and Mongoose/src/config/routes.js
@@ -7,7 +7,7 @@ const accessoryControler = require('../controllers/accessoryController');
 
 router.use(homeController);
 router.use('/cubes', createAddCubeController); //тук си добавихме /cubes, трябва да се оправи пътя и в main layout
-router.use('/accesories', accessoryControler);
+router.use('/accessories', accessoryControler);
 
 router.get('*', (req, res) => {
     res.redirect('/404');

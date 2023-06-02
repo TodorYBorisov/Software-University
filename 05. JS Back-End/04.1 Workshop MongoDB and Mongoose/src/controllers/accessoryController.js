8154c7d5185baaa5b6ff64971334ce3bc9f1c6ef
Todor Borisov <todor0203@gmail.com>
2023-06-02 15:39:09 +0300

    Add post action for create accessory

--- 05. JS Back-End/04.1 Workshop MongoDB and Mongoose/src/controllers/accessoryController.js
+++ 05. JS Back-End/04.1 Workshop MongoDB and Mongoose/src/controllers/accessoryController.js
@@ -2,14 +2,20 @@
 // const router = express.Router();
 
 //това е същото като горното само че на един ред разписано
-const router =require('express').Router();
+const router = require('express').Router();
 
 router.get('/create', (req, res) => {
 
-    res.render('accessory/create');
+    res.render('accessory/create');// така директно рендерираме файла от папката
 
 });
 
+router.post('/create', (req, res) => {
+    const body = req.body;
 
+    console.log(body);
 
+    res.redirect('/');
+
+});
 module.exports = router;

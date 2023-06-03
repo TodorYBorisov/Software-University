e2070294b8a003766465cf6a92dd4ed9faaee00c
Todor Borisov <todor0203@gmail.com>
2023-06-03 22:43:26 +0300

    Update index.js

--- 05. JS Back-End/05. Session and Authentication/cookies/index.js
+++ 05. JS Back-End/05. Session and Authentication/cookies/index.js
@@ -2,13 +2,54 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
 
-    // res.writeHead(200,{
-    //     'Set-Cookie': 'theme=dark'
-    // });
-    res.write('Hello');
-    res.end();
+    const cookies = cookieParser(req);
+    if (req.url == '/') {
+
+        let visited = 0;
+        visited++;
+
+        res.writeHead(200, {
+            'Set-Cookie': `visited=${visited}; httpOnly`
+        });
+        res.write('Hello');
+        res.end();
+    } else {
+        res.writeHead(404);
+        res.end();
+    }
 });
 
-server.listen(5000, () => {
-    console.log('Server is listening on port 5000...');
+server.listen(3000, () => {
+    console.log('Server is listening on port 3000...');
 });
+
+function action(req,res) {
+    const cookies = cookieParser(req);
+    if (req.url == '/') {
+
+        let visited = 0;
+        visited++;
+
+        res.writeHead(200, {
+            'Set-Cookie': `visited=${visited}; httpOnly`
+        });
+        res.write('Hello');
+        res.end();
+}
+
+function cookieParser(req) {
+
+    if (req.headers.cookie) {
+        const cookies = Object.fromEntries(req.headers.cookie
+            .split(';')
+            .map(c => c.trim())
+            .map(c => c.split('=')));
+
+        console.log(cookies);
+
+        return cookies;
+    }
+
+    return {};
+
+}

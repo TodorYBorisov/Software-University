17e00f8f81ce56494bb3844d00ee5857683a92dd
Todor Borisov <todor0203@gmail.com>
2023-06-04 10:04:56 +0300

    Cookie parse add

--- 05. JS Back-End/05. Session and Authentication/cookies/index.js
+++ 05. JS Back-End/05. Session and Authentication/cookies/index.js
@@ -1,55 +1,8 @@
-const http = require('http');
+const app = require('express')();
+app.get('/', (req, res) => {
 
-const server = http.createServer((req, res) => {
-
-    const cookies = cookieParser(req);
-    if (req.url == '/') {
-
-        let visited = 0;
-        visited++;
-
-        res.writeHead(200, {
-            'Set-Cookie': `visited=${visited}; httpOnly`
-        });
-        res.write('Hello');
-        res.end();
-    } else {
-        res.writeHead(404);
-        res.end();
-    }
+    res.send('Hello');
 });
 
-server.listen(3000, () => {
-    console.log('Server is listening on port 3000...');
-});
-
-function action(req,res) {
-    const cookies = cookieParser(req);
-    if (req.url == '/') {
-
-        let visited = 0;
-        visited++;
-
-        res.writeHead(200, {
-            'Set-Cookie': `visited=${visited}; httpOnly`
-        });
-        res.write('Hello');
-        res.end();
-}
-
-function cookieParser(req) {
-
-    if (req.headers.cookie) {
-        const cookies = Object.fromEntries(req.headers.cookie
-            .split(';')
-            .map(c => c.trim())
-            .map(c => c.split('=')));
-
-        console.log(cookies);
-
-        return cookies;
-    }
-
-    return {};
-
-}
+app.listen(3000);
+console.log('Server is listening on a port 3000...');

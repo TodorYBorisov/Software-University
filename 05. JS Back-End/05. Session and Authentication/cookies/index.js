d4d285742490f2f7931e86c3471b3832150768f8
Todor Borisov <todor0203@gmail.com>
2023-06-04 11:05:40 +0300

    Add express-session lib

--- 05. JS Back-End/05. Session and Authentication/cookies/index.js
+++ 05. JS Back-End/05. Session and Authentication/cookies/index.js
@@ -1,13 +1,20 @@
 const app = require('express')();
 const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
 app.use(cookieParser());
+app.use(session({
+    secret: 'my secret code',
+    saveUninitialized: true,
+    resave: false,
+    cookie: { secure: false },
+}));
 
 app.get('/', (req, res) => {
-    console.log(req.cookies);
+    console.log(req.session);
+    req.session.visited = (req.session.visited || 0) + 1;
 
-    res.cookie('cookieParser', '1');
-    res.send('Hello');
+    res.send('Hello. Visit counter: ' + req.session.visited);
 });
 
 app.listen(3000);

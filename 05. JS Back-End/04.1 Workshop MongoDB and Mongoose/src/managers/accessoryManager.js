722d5f8e802bf514947a98a9138e2a58882ab8c4
Todor Borisov <todor0203@gmail.com>
2023-06-05 09:45:41 +0300

    Update accessoryManager.js

--- 05. JS Back-End/04.1 Workshop MongoDB and Mongoose/src/managers/accessoryManager.js
+++ 05. JS Back-End/04.1 Workshop MongoDB and Mongoose/src/managers/accessoryManager.js
@@ -6,3 +6,4 @@ exports.getAll = () => Accessory.find();
 
 //правим ф-я, която взима останалите аксесоари, без тези който ние имаме
 exports.getRest = (accessoryId) => Accessory.find({ _id: { $nin: accessoryId } });
+

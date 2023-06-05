3d87f7a5be9c23a0f8bd35c0d9e29d0b23ff9f4f
Todor Borisov <todor0203@gmail.com>
2023-06-05 09:47:16 +0300

    Update hashing.js

--- 05. JS Back-End/05. Session and Authentication/cookies/hashing.js
+++ 05. JS Back-End/05. Session and Authentication/cookies/hashing.js
@@ -5,5 +5,5 @@ const salt = 5;
 const hashPass = bcrypt.hashSync(pass, salt);
 
 
-const resultCompare = bcrypt.compareSync(pass,'$2b$05$FhwgU9c0WlS6yHY9CGGZle2KL.dJrsQhHJubn1SkZsP3w9ZkWH8Hq');
-console.log(resultCompare);
+const passMatch = bcrypt.compareSync(pass,'$2b$05$FhwgU9c0WlS6yHY9CGGZle2KL.dJrsQhHJubn1SkZsP3w9ZkWH8Hq');
+console.log(passMatch);

d47c39cec59105d95576663c46ba051815fde6ba
Todor Borisov <todor0203@gmail.com>
2023-06-07 20:46:16 +0300

    Update User.js

--- 05. JS Back-End/06.1 Workshop Validation and Error Handling/src/models/User.js
+++ 05. JS Back-End/06.1 Workshop Validation and Error Handling/src/models/User.js
@@ -4,13 +4,26 @@ const userSchema = new mongoose.Schema({
 
     username: {
         type: String,
-        require: true
+        require: true,
+        minLength: 5,
+        match: [/^[A-Za-z0-9]+$/,'User must be alphnumeric'],
+        unique: true,
+
     },
     hashedPassword: {
         type: String,
         required: true,
+        validate: {
+            validator: function (value) {
+               return /^[A-Za-z0-9]+$/.test(value);
+            },
+            message: 'Invalid password chars!'
+        },
+        minLength: 8,
     },
 });
+
+userSchema.index({ username: 1 }, { collation: { locale: 'en', strength: 2 } });
 
 //валидиране на репеат паролата , че е същата като паролата
 // userSchema.virtual('repeatPassword')
@@ -24,3 +37,7 @@ const userSchema = new mongoose.Schema({
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
+
+
+//условие ако искаме username да е уникален, unique:true се добавя при username
+//userSchema.index({ username: 1 }, { collation: { locale: 'en', strength: 2 } });

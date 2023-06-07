570711d3e1a1cfc84d6080f8b1c23776c94d2a5c
Todor Borisov <todor0203@gmail.com>
2023-06-07 12:07:49 +0300

    Update routGuards.js

--- 05. JS Back-End/05.1 Workshop Session and Authentication/src/middlewares/routGuards.js
+++ 05. JS Back-End/05.1 Workshop Session and Authentication/src/middlewares/routGuards.js
@@ -19,6 +19,23 @@ function isGuest() {
 }
 
 module.exports = {
-    hasUser,
-    isGuest,
+	hasUser,
+	isGuest,
+};
+
+// function isAuth() {
+// 	return (req, res, next) =>{
+// 		if(!req.user){
+// 			res.redirect('/users/login');
+// 		}else{
+// 			next();
+// 		}
+// 	};
+// }
+
+exports.isAuth = (req, res, next) => {
+	if (!req.user) {
+		return res.redirect('/users/login');
+	}
+	next();
 };

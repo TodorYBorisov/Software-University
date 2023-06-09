5380e2407b7aa8d6a57ab449819d97e1782efc37
Todor Borisov <todor0203@gmail.com>
2023-06-09 16:16:09 +0300

    Create routesConfig.js

---
+++ 05. JS Back-End/Exam prep/Booking Uni/config/routesConfig.js
@@ -0,0 +1,20 @@
+const homeController = require('../controllers/homeController');
+const authController = require('../controllers/authController');
+//============================================================//
+const hotelController = require('../controllers/hotelController');
+const profileController = require('../controllers/profileController');
+
+
+module.exports = (app) => {
+
+    app.use('/', homeController);
+    app.use('/auth', authController);
+    app.use('/hotel', hotelController);
+    app.use('/profile', profileController);
+
+
+    // app.all('*', (req, res) => {
+    //     res.render('404');
+    // });
+
+};

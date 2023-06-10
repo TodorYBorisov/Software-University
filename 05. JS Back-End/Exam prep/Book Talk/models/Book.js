92bc90e0eeb24b0c52a5fad2fc15fef96a59864e
Todor Borisov <todor0203@gmail.com>
2023-06-10 22:01:15 +0300

    Create Book.js

---
+++ 05. JS Back-End/Exam prep/Book Talk/models/Book.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+
+const pattern = /^https?:\/\/.+$/gi;
+const bookSchema = new mongoose.Schema({
+	title: {
+		type: String,
+		required: [true, 'Title is required!'],
+		minLength: [2, 'The Title should be at least 2 characters!'],
+
+		//match: [patternUser, 'Username should consist english letters and digits only. Plese try again']
+	},
+	author: {
+		type: String,
+		required: [true, 'Author is required!'],
+		minLength: [5, 'The Author should be at least 5 characters!'],
+
+		//match: [patternEmail, 'Email should consist english letters and digits only. Plese try again']
+	},
+	imageUrl: {
+		type: String,
+		required: [true, 'Image is required!'],
+		match: [pattern, 'The Image should start with http:// or https://'],
+	},
+	review: {
+		type: String,
+		required: [true, 'Review is required!'],
+		minLength: [10, 'The Review should be a minimum of 10 characters long!'],
+	},
+	genre: {
+		type: String,
+		required: [true, 'Genre is required!'],
+		minLength: [3, 'The Genre should be at least 3 characters!'],
+	},
+	stars: {
+		type: Number,
+		required: [true, 'Stars is required!'],
+		min: [1, 'The Stars should be a positive number between 1 and 5!'],
+		max: [5, 'The Stars should be a positive number between 1 and 5!'],
+	},
+	owner: {
+		type: mongoose.Types.ObjectId,
+		ref: 'User'
+		//Owner - object Id (a reference to the User model)
+	},
+	wishingList: {
+		type: [mongoose.Types.ObjectId],
+		ref: 'User'
+		//WishingList – a collection of Users (a reference to the User model)
+	},
+
+});
+
+const Book = mongoose.model('Book', bookSchema);
+
+module.exports = Book;
+
+
+
+

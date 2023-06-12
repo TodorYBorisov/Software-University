c8fa2fa9928dd45d64f4c50edef7a55e61da55f9
Todor Borisov <todor0203@gmail.com>
2023-06-12 14:38:52 +0300

    Create session.js

---
+++ 05. JS Back-End/Exam prep/Crypto Trade/middlewares/session.js
@@ -0,0 +1,28 @@
+const { verifyToken } = require('../services/userService');
+
+module.exports = () => (req, res, next) => {
+	const token = req.cookies.token;
+
+	if (token) {
+		try {
+			const userData = verifyToken(token);
+            req.user = userData;
+
+			res.locals.username = userData.username;
+			res.locals.email = userData.email;
+		} catch (error) {
+            res.clearCookie('token');
+            res.redirect('/auth/login');
+            return;
+        }
+	}
+
+	next();
+};
+
+//1 няма cookie/token
+//2 има cookie/token
+//3.има невалидно cookie
+//С res.locals.username по този начин вкарваме username в глобалния контекст през мидълуера, който темплета ще го чете
+
+// ако искаме да добавим други полета в res.locals трябва да ги подадем в payload в userService

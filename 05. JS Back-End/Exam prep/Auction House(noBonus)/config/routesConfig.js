d3fe65761f1639c82c09640be9ee1fe565d8b064
Todor Borisov <todor0203@gmail.com>
2023-06-13 23:51:40 +0300

    Create routesConfig.js

---
+++ 05. JS Back-End/Exam prep/Auction House(noBonus)/config/routesConfig.js
@@ -0,0 +1,24 @@
+const homeController = require('../controllers/homeController');
+const authController = require('../controllers/authController');
+//============================================================//
+// const bookController = require('../controllers/bookController');
+// const profileController = require('../controllers/profileController');
+// const searchController = require('../controllers/searchController');
+const auctionController = require('../controllers/auctionController');
+
+
+module.exports = (app) => {
+
+    app.use('/', homeController);
+    app.use('/auth', authController);
+    //app.use('/books', bookController); // да си добавя актуалните контролери
+    //app.use('/profile', profileController); // да си добавя актуалните контролери
+    //app.use('/search', searchController); // да си добавя актуалните контролери
+    app.use('/auctions', auctionController); // да си добавя актуалните контролери
+
+
+    app.all('*', (req, res) => {
+        res.render('404');
+    });
+
+};

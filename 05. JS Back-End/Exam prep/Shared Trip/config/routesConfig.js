f71a0ac8baca51f238a09ad25ed006870eaf2c89
Todor Borisov <todor0203@gmail.com>
2023-06-14 22:40:26 +0300

    Create routesConfig.js

---
+++ 05. JS Back-End/Exam prep/Shared Trip/config/routesConfig.js
@@ -0,0 +1,23 @@
+const homeController = require('../controllers/homeController');
+const authController = require('../controllers/authController');
+//============================================================//
+const tripController = require('../controllers/tripController');
+// const profileController = require('../controllers/profileController');
+// const searchController = require('../controllers/searchController');
+
+
+
+module.exports = (app) => {
+
+    app.use('/', homeController);
+    app.use('/auth', authController);
+    app.use('/trips', tripController); // да си добавя актуалните контролери
+    //app.use('/profile', profileController); // да си добавя актуалните контролери
+    //app.use('/search', searchController); // да си добавя актуалните контролери
+
+
+    app.all('*', (req, res) => {
+        res.render('404');
+    });
+
+};

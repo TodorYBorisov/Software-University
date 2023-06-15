1646cd79c4bca149a49ba1e038fe49a930bd2167
Todor Borisov <todor0203@gmail.com>
2023-06-15 19:34:02 +0300

    Add search functionality

---
+++ 05. JS Back-End/Exam prep/Real Estate Agency/controllers/searchController.js
@@ -0,0 +1,28 @@
+const { hasUser } = require('../middlewares/guards');
+const { search } = require('../services/estateService'); //ТУК  да проверя дали го зарежда от правилното място сега идва от bookService
+const { parseError } = require('../util/parser');
+
+const searchController = require('express').Router(); // Правим си самия контролер
+
+//ТУК ДА СЕ СМЕНИ С КОНТРОЛЕРА ОТ ЗАДАНИЕТО!!!
+searchController.get('/', hasUser(), async (req, res) => {
+    try {
+        const estates = await search(req.query.search);
+
+        res.render('search', {
+            title: 'Search Page',
+            query: req.query.search,
+            estates
+        });
+    } catch (error) {
+        res.render('404', {
+            errors: parseError(error),
+            title: 'Search Page',
+            query: req.query.search,
+        });
+    }
+
+});
+
+module.exports = searchController;
+

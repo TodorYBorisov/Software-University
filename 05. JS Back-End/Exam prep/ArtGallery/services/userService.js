75aa03d876081481fd97844cf0f2a3eb2c1f7c38
Todor Borisov <todor0203@gmail.com>
2023-06-16 15:09:53 +0300

    Create userService.js

---
+++ 05. JS Back-End/Exam prep/ArtGallery/services/userService.js
@@ -0,0 +1,77 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const SECRET = 'ea4e9079925db2d565eccd34766c8531cf21d27eeb531218167ad5876078ac26';
+
+async function register(username, address, password) { // тук след регистрация да видя на къде се редиректва в задачата
+
+    const existingUsername = await User.findOne({ username }); // Проверяваме по username, дали вече има такъв в User базата
+    if (existingUsername) {                                     // Ако намерим съвпадение
+        throw new Error('Username or address already exists!');   // Хвърляме грешка
+    }
+
+    // const existingEmail = await User.findOne({ email }).collation({ locale: 'en', strength: 2 });        // Проверяваме по email, дали вече има такъв
+    // if (existingEmail) {                                        // Ако намерим съвпадение
+    //     throw new Error('Username or email already exists!');   // Хвърляме грешка
+    // }
+    if (password.length < 3) {                                   // Валидираме дължината на паролата
+        throw new Error('Password is too short!');               // Хвърляме грешка
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);  // Хешираме паролата, като подаваме паролата във вида подадена от потребителя и я осоляваме 10 пъти
+
+    const user = await User.create({    //Създаваме нов потребител в базата данни с email, username,  и hashedPassword 
+        username,
+        address,
+        hashedPassword
+    });
+
+    //ТУК ДА ВИДИМ ДАЛИ РЕГИСТРИРАНЕТО ТРЯБВА ДА СЪЗДАДЕ СЕСИЯ
+    return createSession(user);
+}
+
+async function login(username, password) {
+
+    if (password.length < 3) {                      // Ако има валидация за дължина на паралота
+        throw new Error('Password is too short!'); // Хвърляме грешка
+    }
+
+    const user = await User.findOne({ username });  // Проверяваме по username, дали вече има такъв
+    if (!user) {                                             // Ако няма регистриран такъв user
+        throw new Error('Incorrect username or password!');    // Хвърляме грешка 
+    }
+
+    const hasMatch = await bcrypt.compare(password, user.hashedPassword);   // Сравняваме подадената парола с хешираната от намерения user 
+
+    if (!hasMatch) {                                             // Ако няма съвпадение 
+        throw new Error('Incorrect username or password!');        // Хвърляме грешка 
+    }
+
+    return createSession(user);
+}
+
+function createSession({ _id, username, address }) { //това е user обекта който сме направили по горе
+    const payload = {  // Това са данните, които искаме да запазим в токена        
+        _id,
+        username,
+        address
+    };
+
+    const token = jwt.sign(payload, SECRET, {expiresIn: '2d'}); // Връщаме подписан токена, ако искаме да изтича добавяме 
+    return token;                           //{expiresIn: '2d'} ако искаме да изтича токена го подаваме след secter
+}
+
+function verifyToken(token) {
+    return jwt.verify(token, SECRET);
+}
+
+module.exports = {
+    register,
+    login,
+    verifyToken
+};
+
+
+// За secret => Изпълнява се в терминала и генерира 64 bit ASCII string
+// node -e "console.log(require('crypto').randomBytes(32).toString('hex'))"

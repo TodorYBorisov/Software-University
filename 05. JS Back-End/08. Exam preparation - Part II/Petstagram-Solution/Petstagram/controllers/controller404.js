6d6f0512eb33484b92a2ec2fa7cd64c1002c93e1
Todor Borisov <todor0203@gmail.com>
2023-06-17 19:43:14 +0300

    Updated with new files

--- 05. JS Back-End/08. Exam preparation - Part II/Petstagram-Solution/Petstagram/controllers/controller404.js
+++
@@ -1,9 +0,0 @@
-const controller404 = require('express').Router();
-
-controller404.get('*', (req, res) => {
-    res.render('404', {
-        title: 'Petstagram',
-    });
-});
-
-module.exports = { controller404 }; 

549071ff39bb01fed84035730a6da8cb604fd703
Todor Borisov <todor0203@gmail.com>
2023-06-27 11:08:07 +0300

    Update test.ts

--- 06. Front-End Framework - Angular/01. Intro to Angular and TypeScript/TypeScriptDemo/test.ts
+++ 06. Front-End Framework - Angular/01. Intro to Angular and TypeScript/TypeScriptDemo/test.ts
@@ -1,1 +1,33 @@
 let isEven: boolean = false;
+
+type User = {
+    name: String,
+    age: Number,
+}
+
+const peshoUser = {
+    name: 'Pesho',
+    age: 25
+} as User;
+
+interface anotherUser { //описва формата на обекта, какви типове могат да се ползват
+    firstName: string,
+    id: number,
+}
+
+class anotherUserCalss implements anotherUser { // за да не се чупи трябва да декларираме пропъртитата от интерфейса
+    firstName: string;
+    id: number;
+
+}
+
+const anotherUserList = [
+    { firstName: 'Ivan', id: 1 },
+    { firstName: 'Toshko', id: 2 },
+    { firstName: 'Ivo', id: 3 }
+] as anotherUser[];
+
+anotherUserList.forEach(({ firstName, id }) => {
+
+    console.log(`${id}: ${firstName}`);
+});

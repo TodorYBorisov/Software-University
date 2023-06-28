962e712fd745a70ea416cd4bce2e9d1783abf525
Todor Borisov <todor0203@gmail.com>
2023-06-28 15:43:26 +0300

    Update test.ts

--- 06. Front-End Framework - Angular/01. Intro to Angular and TypeScript/TypeScriptDemo/test.ts
+++ 06. Front-End Framework - Angular/01. Intro to Angular and TypeScript/TypeScriptDemo/test.ts
@@ -1,76 +1,76 @@
-let isEven: boolean = false;
-let num: number = 5; //  така декларираме число
-let str: string = '';//така декларираем стрингове
-let color: string = 'blue';
-color = 'red';
+// let isEven: boolean = false;
+// let num: number = 5; //  така декларираме число
+// let str: string = '';//така декларираем стрингове
+// let color: string = 'blue';
+// color = 'red';
 
-let list: number[] = [1, 2, 3, 4, 5];
-let list2: Array<number> = [1, 2, 3];
-
-
-type User = {
-    name: String,
-    age: Number,
-}
-
-const peshoUser = {
-    name: 'Pesho',
-    age: 25
-} as User;
-
-interface anotherUser { //описва формата на обекта, какви типове могат да се ползват
-    firstName: string,
-    id: number,
-}
-
-class anotherUserCalss implements anotherUser { // за да не се чупи трябва да декларираме пропъртитата от интерфейса
-    firstName: string;
-    id: number;
-
-}
-
-const anotherUserList = [
-    { firstName: 'Ivan', id: 1 },
-    { firstName: 'Toshko', id: 2 },
-    { firstName: 'Ivo', id: 3 }
-] as anotherUser[];
-
-anotherUserList.forEach(({ firstName, id }) => {
-
-    console.log(`${id}: ${firstName}`);
-});
+// let list: number[] = [1, 2, 3, 4, 5];
+// let list2: Array<number> = [1, 2, 3];
 
 
-class Human {
+// // type User = {
+// //     name: String,
+// //     age: Number,
+// // }
 
-    speak() {
-        console.log('Hello');
+// const peshoUser = {
+//     name: 'Pesho',
+//     age: 25
+// } as User;
 
-    }
-}
+// interface anotherUser { //описва формата на обекта, какви типове могат да се ползват
+//     firstName: string,
+//     id: number,
+// }
 
-class Person extends Human {
-    private name: string;
-    public age: number;
+// class anotherUserCalss implements anotherUser { // за да не се чупи трябва да декларираме пропъртитата от интерфейса
+//     firstName: string;
+//     id: number;
 
-    constructor(name: string, age: number) {
-        super();
-        this.name = name;
-        this.age = age
+// }
 
-    }
-}
+// const anotherUserList = [
+//     { firstName: 'Ivan', id: 1 },
+//     { firstName: 'Toshko', id: 2 },
+//     { firstName: 'Ivo', id: 3 }
+// ] as anotherUser[];
 
-const personPesho = new Person('Ivan', 33);
+// anotherUserList.forEach(({ firstName, id }) => {
 
-function getIdentity<T>(id:T) {
-    console.log(id);
-}
-getIdentity<string>('123');
-getIdentity<number>(123)
+//     console.log(`${id}: ${firstName}`);
+// });
 
-enum PaymentStatus {
-    Fauled,
-    Successfull,
-    Pending
-}
+
+// class Human {
+
+//     speak() {
+//         console.log('Hello');
+
+//     }
+// }
+
+// class Person extends Human {
+//     private name: string;
+//     public age: number;
+
+//     constructor(name: string, age: number) {
+//         super();
+//         this.name = name;
+//         this.age = age
+
+//     }
+// }
+
+// const personPesho = new Person('Ivan', 33);
+
+// function getIdentity<T>(id:T) {
+//     console.log(id);
+// }
+// getIdentity<string>('123');
+// getIdentity<number>(123)
+
+// enum PaymentStatus {
+//     Fauled,
+//     Successfull,
+//     Pending
+// }

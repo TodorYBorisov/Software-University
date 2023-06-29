1dd280961dca72c4765abca8b71dd81d2017c85e
Todor Borisov <todor0203@gmail.com>
2023-06-29 14:05:28 +0300

    Update navigation.component.ts

--- 06. Front-End Framework - Angular/02. Components/components-demo-app/src/app/navigation/navigation.component.ts
+++ 06. Front-End Framework - Angular/02. Components/components-demo-app/src/app/navigation/navigation.component.ts
@@ -1,14 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css']
 })
-export class NavigationComponent {
+export class NavigationComponent implements OnInit {
 
-  title ='Navigation title';
-  onClick() {
-    console.log('This button was clicked!');
+  activeUsers = [
+    { name: 'Mitko', age: 21 },
+    { name: 'Ivo', age: 18 },
+    { name: 'Toshko', age: 25 }
+  ]
+
+  isActive = false;
+  inputValue ='Hello'
+
+  ngOnInit(): void {
+    console.log('Navigation->created');
+    
+  }
+
+  ngOnChanges(changes: SimpleChanges){
+    console.log(changes);
+    
+
+  }
+
+
+  onClick(event:Event): void {
+    console.log({event});
+
+    //...args: number[] така подаваме аргументи в ивента
+    
+    this.isActive = !this.isActive;
   }
 }

4863b7e09df10865cffca83dfed0e1093e6d722e
Todor Borisov <todor0203@gmail.com>
2023-06-30 11:36:05 +0300

    Update app.component.ts

--- 06. Front-End Framework - Angular/02. Components/components-demo-app/src/app/app.component.ts
+++ 06. Front-End Framework - Angular/02. Components/components-demo-app/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Title from Appcomponent!';
+  title = 'This text is from app main app componet!';
 
   users = [
     { name: 'Mitko', age: 21 },

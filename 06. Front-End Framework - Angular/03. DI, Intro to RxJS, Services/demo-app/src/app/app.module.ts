2f88f35750abedbeccb84ab43eff2617e434a5b2
Todor Borisov <todor0203@gmail.com>
2023-06-30 16:48:21 +0300

    Add user list and list of items

--- 06. Front-End Framework - Angular/03. DI, Intro to RxJS, Services/demo-app/src/app/app.module.ts
+++ 06. Front-End Framework - Angular/03. DI, Intro to RxJS, Services/demo-app/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UsersListComponent } from './users-list/users-list.component';
+import { UserListItemComponent } from './user-list-item/user-list-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersListComponent,
+    UserListItemComponent
   ],
   imports: [
     BrowserModule

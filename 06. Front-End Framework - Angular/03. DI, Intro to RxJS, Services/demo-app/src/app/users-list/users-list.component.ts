a1f51c03e797b1a29a2d1ca14b8d01b921c09ecb
Todor Borisov <todor0203@gmail.com>
2023-07-01 17:18:58 +0300

    Update users-list.component.ts

--- 06. Front-End Framework - Angular/03. DI, Intro to RxJS, Services/demo-app/src/app/users-list/users-list.component.ts
+++ 06. Front-End Framework - Angular/03. DI, Intro to RxJS, Services/demo-app/src/app/users-list/users-list.component.ts
@@ -1,13 +1,29 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input } from '@angular/core';
 import { User } from '../types/User';
 
 @Component({
   selector: 'app-users-list',
   templateUrl: './users-list.component.html',
-  styleUrls: ['./users-list.component.css']
+  styleUrls: ['./users-list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+
 })
 export class UsersListComponent {
 
   @Input() users: User[] =[];
 
+    //взимане на change detectora
+    constructor(private cd: ChangeDetectorRef) {
+
+    }
+  
+    ngOnChanges() {
+      console.log('Invoked from ngOnChanges');
+  
+    }
+  
+    //правим функция която да извиква промените
+    refresh() {
+      this.cd.detectChanges();
+    }
 }

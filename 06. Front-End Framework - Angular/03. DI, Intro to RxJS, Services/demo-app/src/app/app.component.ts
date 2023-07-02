50ede5a2ed94943719b6798fed58bfe252374865
Todor Borisov <todor0203@gmail.com>
2023-07-02 13:07:48 +0300

    Update app.component.ts

--- 06. Front-End Framework - Angular/03. DI, Intro to RxJS, Services/demo-app/src/app/app.component.ts
+++ 06. Front-End Framework - Angular/03. DI, Intro to RxJS, Services/demo-app/src/app/app.component.ts
@@ -17,27 +17,77 @@ export class AppComponent {
     { name: 'Toshko', age: 30 },
   ]
 
-  constructor(){
-    setInterval(()=>{
+  constructor() {
+    setInterval(() => {
       this.users.push({
-        name:'DemoName',
-        age:0
+        name: 'DemoName',
+        age: 0
       })
       //console.log('User has been added!');
-      
+
     }) //,3000 
   }
 
   addUser(inputName: HTMLInputElement, inputAge: HTMLInputElement) {
-    
+
     const user = {
       name: inputName.value,
       age: Number(inputAge.value)
     }
     this.users.push(user)
     // this.users = [...this.users, user] // сменяме референцията, за да може да тригърнем чендж детекшъна, презаписваме
-    inputName.value=''
-    inputAge.value=''
+    inputName.value = ''
+    inputAge.value = ''
 
   }
 }
+//dependancy injection example
+
+class Wallet {
+  ballance = 0;
+  name =''
+
+  constructor(ballance: number, name: string) {
+    this.ballance = ballance
+    this.name = name
+  }
+}
+class Car {
+  model = '';
+  constructor(model: string) {
+    this.model = model;
+
+  }
+}
+
+// class Person {
+//   wallet: Wallet;
+//   car:Car;
+
+//   constructor(name:string, balance: number, carModel:string) {
+//     this.car = new Car(carModel);
+//     this.wallet = new Wallet(name, balance)
+//   }
+// }
+
+
+// const personIvan = new Person(4000);
+// personIvan.wallet.ballance //4000
+// const personMaria = new Person(5000)
+// personMaria.wallet.ballance //5000
+
+class PersonBetter {
+  wallet:Wallet;
+  car:Car;
+
+  constructor(car:Car, wallet:Wallet){
+    this.car=car;
+    this.wallet=wallet
+  }
+}
+
+//инстанцираме всичко отвън и подаваме самите интснации ан самите калсове
+const ivansCar =new Car('BMW');
+const ivansWallet = new Wallet(5000,'Ivan');
+
+const betterPerson = new PersonBetter(ivansCar,ivansWallet)

cab67a29b06c56ee0ebda292423fee63b3c7e13b
Todor Borisov <todor0203@gmail.com>
2023-07-08 12:47:38 +0300

    Update app.module.ts

--- 06. Front-End Framework - Angular/05. Modules and Routing/Routing demo/routing/src/app/app.module.ts
+++ 06. Front-End Framework - Angular/05. Modules and Routing/Routing demo/routing/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 import { CoreModule } from './core/core.module';
 import { UserModule } from './user/user.module';
@@ -15,7 +16,8 @@ import { UserModule } from './user/user.module';
     BrowserModule,
     AppRoutingModule,
     CoreModule,
-    UserModule
+    UserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]

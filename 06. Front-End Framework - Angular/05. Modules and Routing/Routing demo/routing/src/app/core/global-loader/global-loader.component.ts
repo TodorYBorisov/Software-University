203cab1e99cea13d7cd52e68846f2b0451006fc4
Todor Borisov <todor0203@gmail.com>
2023-07-08 14:15:50 +0300

    Update files

--- 06. Front-End Framework - Angular/05. Modules and Routing/Routing demo/routing/src/app/core/global-loader/global-loader.component.ts
+++ 06. Front-End Framework - Angular/05. Modules and Routing/Routing demo/routing/src/app/core/global-loader/global-loader.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { GlobalLoaderService } from './global-loader.service';
+
 
 @Component({
   selector: 'app-global-loader',
@@ -6,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./global-loader.component.css']
 })
 export class GlobalLoaderComponent {
+
+  constructor(public globalLoaderService: GlobalLoaderService) { }
+  
 
 }
--- 06. Front-End Framework - Angular/05. Modules and Routing/Routing demo/routing/src/app/user/user-list/user-list.component.ts
+++ 06. Front-End Framework - Angular/05. Modules and Routing/Routing demo/routing/src/app/user/user-list/user-list.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user.service';
 import { User } from 'src/app/types/User';
+import { GlobalLoaderService } from 'src/app/core/global-loader/global-loader.service';
+
+
 
 @Component({
   selector: 'app-user-list',
@@ -11,13 +14,49 @@ export class UserListComponent implements OnInit {
 
   userList: User[] = [];
 
-  constructor(private userService: UserService) { }
+  constructor(
+    private userService: UserService,
+    private globalLoaderService: GlobalLoaderService) { }
 
-  ngOnInit(): void {
-    this.userService.fetchUsers().subscribe((users) => {
-      this.userList = users
-      console.log(users);
-
-    });
+    //Toва е правилният начин с error handling
+    ngOnInit(): void {
+      
+      this.globalLoaderService.showLoader();
+      
+      // setTimeout(() => {
+        
+        this.userService.fetchUsers().subscribe({
+          next: (users) => {
+            this.userList = users;
+            this.globalLoaderService.hideLoader()
+          },
+          error: (error) => {
+            console.error(`Error: ${error}`);
+            this.globalLoaderService.hideLoader();
+          }
+        });
+      // }, 1000);
+    }
+    
+    // ngOnInit(): void {
+  
+    //   this.globalLoaderService.showLoader()
+  
+    //   //така тестваме лоудара дали се показва на 1 сек преди да се заредят данните
+    //   // setTimeout(() => {
+  
+    //   this.userService.fetchUsers().subscribe((users) => {
+    //     this.userList = users
+    //     this.globalLoaderService.hideLoader()
+    //   });
+  
+    //   this.userService.fetchUsers().subscribe((users) => {
+    //     this.userList = users
+    //     //console.log(users);
+  
+    //     this.globalLoaderService.hideLoader();
+    //   });
+  
+    //   // },1000);
+    // }
   }
-}

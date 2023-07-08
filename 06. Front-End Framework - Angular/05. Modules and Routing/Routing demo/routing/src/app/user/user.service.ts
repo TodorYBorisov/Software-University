18c9afdb63568f266dab2c494748d535e561aa49
Todor Borisov <todor0203@gmail.com>
2023-07-08 14:18:55 +0300

    Update user.service.ts

--- 06. Front-End Framework - Angular/05. Modules and Routing/Routing demo/routing/src/app/user/user.service.ts
+++ 06. Front-End Framework - Angular/05. Modules and Routing/Routing demo/routing/src/app/user/user.service.ts
@@ -5,13 +5,10 @@ import { User } from '../types/User';
 @Injectable({
   providedIn: 'root'
 })
-export class UserService implements OnInit {
+export class UserService {
 
   constructor(private http: HttpClient) { }
-  ngOnInit(): void {
-    //
-  }
-
+  
   fetchUsers() {
     return this.http.get<User[]>('https://jsonplaceholder.typicode.com/users');
   }

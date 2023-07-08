6e0e165b3ce910dc202b50555be207ac085c8a45
Todor Borisov <todor0203@gmail.com>
2023-07-08 12:48:05 +0300

    Update user-list.component.ts

--- 06. Front-End Framework - Angular/05. Modules and Routing/Routing demo/routing/src/app/user/user-list/user-list.component.ts
+++ 06. Front-End Framework - Angular/05. Modules and Routing/Routing demo/routing/src/app/user/user-list/user-list.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
+import { User } from 'src/app/types/User';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css']
 })
-export class UserListComponent {
+export class UserListComponent implements OnInit {
 
+  myFetchData: User[] = [];
+  
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.userService.fetchUsers().subscribe((users) => {
+      this.myFetchData =users
+      console.log(users);
+
+    });
+  }
 }

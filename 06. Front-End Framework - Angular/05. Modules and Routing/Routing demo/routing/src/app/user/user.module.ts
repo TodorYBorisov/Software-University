2e4cf5b9da05fb19f68e00cf9c1e591a09aa7259
Todor Borisov <todor0203@gmail.com>
2023-07-10 00:11:48 +0300

    Update user.module.ts

--- 06. Front-End Framework - Angular/05. Modules and Routing/Routing demo/routing/src/app/user/user.module.ts
+++ 06. Front-End Framework - Angular/05. Modules and Routing/Routing demo/routing/src/app/user/user.module.ts
@@ -4,6 +4,7 @@ import { UserListComponent } from './user-list/user-list.component';
 import { RouterModule } from '@angular/router';
 import { UserDetailComponent } from './user-detail/user-detail.component';
 import { UserResolver } from './user-detail/user-detail.resolver';
+import { AuthGuard } from './user-detail/user-detail.guard';
 
 
 
@@ -22,6 +23,7 @@ import { UserResolver } from './user-detail/user-detail.resolver';
       {
         path: 'user/detail/:id',
         resolve: { user: UserResolver },
+        canActivate: [AuthGuard],
         component: UserDetailComponent
       },
 

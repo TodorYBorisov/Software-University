ceef5ce9267fa398040964294c41461a14d61c44
Todor Borisov <todor0203@gmail.com>
2023-07-22 16:24:49 +0300

    Update auth.service.ts

--- 06. Front-End Framework - Angular/09. Pipes, Interceptors and Subjects/demo/src/app/auth.service.ts
+++ 06. Front-End Framework - Angular/09. Pipes, Interceptors and Subjects/demo/src/app/auth.service.ts
@@ -1,15 +1,34 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  private userSubject$$ = new BehaviorSubject<Object | null>(null);
+  private isLoading$$ = new BehaviorSubject<boolean>(false);
+
+
+
+  public userObject$ = this.userSubject$$.asObservable()
+  public isLoadingUsers$ = this.isLoading$$.asObservable()
+
   constructor(private htpp: HttpClient) { }
 
-  loadUsers() {
+  loadUsers(): void {
 
-    return this.htpp.get('/api/users');
+    this.userSubject$$.next(null);
+    this.isLoading$$.next(true) // да стартира лоудъра
+
+    this.htpp.get('/api/users').subscribe({
+      next: (users) => {
+        this.isLoading$$.next(false)
+        this.userSubject$$.next(users)
+      },
+    });
   }
+
+
 }

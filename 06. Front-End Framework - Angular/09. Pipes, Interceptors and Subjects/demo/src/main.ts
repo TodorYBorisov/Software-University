9c6d7e60be9fa4682453b6facf7278201b868e26
Todor Borisov <todor0203@gmail.com>
2023-07-22 16:24:54 +0300

    Update main.ts

--- 06. Front-End Framework - Angular/09. Pipes, Interceptors and Subjects/demo/src/main.ts
+++ 06. Front-End Framework - Angular/09. Pipes, Interceptors and Subjects/demo/src/main.ts
@@ -1,7 +1,30 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { BehaviorSubject, Subject } from 'rxjs';
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+
+
+// subjects
+
+const subj$$ = new Subject();
+
+subj$$.subscribe(console.log);
+subj$$.next(123);
+subj$$.subscribe(console.log);
+subj$$.next(1234);
+subj$$.next(12345);
+
+subj$$.subscribe({
+  next: console.log,
+  error: (error) => console.log(`Error: ${error}`),
+  complete: () => console.log(`Obs complete`)
+});
+
+const bSubj$$ = new BehaviorSubject(100);
+bSubj$$.subscribe(console.log)
+bSubj$$.next(101)

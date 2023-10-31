aed21ad8b0c8912a52bcd7a6c6f672de69539305
Todor Borisov <todor0203@gmail.com>
2023-10-31 17:19:35 +0200

    Update App.jsx

--- 08. ReactJS - Oct 2023/02. Components Basic Idea/Demo 2/componetDemo/src/App.jsx
+++ 08. ReactJS - Oct 2023/02. Components Basic Idea/Demo 2/componetDemo/src/App.jsx
@@ -1,14 +1,17 @@
 import './App.css';
 import movies from './assets/movies';
 import MovieList from './components/MovieList';
+import Timer from './components/Timer';
 
 
 function App() {
 
   return (
     <div>
+      <Timer />
       <h1>My first dynamic react application!</h1>
       <MovieList movies={movies} headingText='My movie list' />
+
 
     </div>
   )

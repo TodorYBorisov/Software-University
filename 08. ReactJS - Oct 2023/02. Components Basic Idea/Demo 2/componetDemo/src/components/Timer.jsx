ea109c2d8430c7dd57177455e2a74cac61369cbd
Todor Borisov <todor0203@gmail.com>
2023-10-31 17:22:17 +0200

    Update Timer.jsx

--- 08. ReactJS - Oct 2023/02. Components Basic Idea/Demo 2/componetDemo/src/components/Timer.jsx
+++ 08. ReactJS - Oct 2023/02. Components Basic Idea/Demo 2/componetDemo/src/components/Timer.jsx
@@ -2,10 +2,10 @@ import { useState } from "react";
 
 export default function Timer(props) {
 
-    const [time, setTime] = useState(0);//ползваме array destructuring assignment и задаваме начлана стойност от 0
+    const [time, setTime] = useState(props.startTime);//ползваме array destructuring assignment и задаваме начлана стойност от 0
 
     // console.log(time);
-    
+
     //useEffect instead of setTime
     setTimeout(() => {
         setTime(time + 1);

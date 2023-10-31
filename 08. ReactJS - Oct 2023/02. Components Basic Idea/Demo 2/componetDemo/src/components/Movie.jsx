f4bfd93653f8b3492362caa2fd040bc1d7c3cd73
Todor Borisov <todor0203@gmail.com>
2023-10-31 11:29:12 +0200

    Update Movie.jsx

--- 08. ReactJS - Oct 2023/02. Components Basic Idea/Demo 2/componetDemo/src/components/Movie.jsx
+++ 08. ReactJS - Oct 2023/02. Components Basic Idea/Demo 2/componetDemo/src/components/Movie.jsx
@@ -1,8 +1,8 @@
 export default function Movie(props) {
     return (
-        <article>
+        <li>
             <h3>{props.data.title}</h3>
             <p>{props.data.id}</p>
-        </article>
+        </li>
     );
 };

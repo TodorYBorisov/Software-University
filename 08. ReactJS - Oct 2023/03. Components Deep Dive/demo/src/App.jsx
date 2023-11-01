4cc1d8defb0800f059cfcc59f666d57dca80761d
Todor Borisov <todor0203@gmail.com>
2023-11-01 17:55:51 +0200

    Update App.jsx

--- 08. ReactJS - Oct 2023/03. Components Deep Dive/demo/src/App.jsx
+++ 08. ReactJS - Oct 2023/03. Components Deep Dive/demo/src/App.jsx
@@ -1,12 +1,27 @@
-import './App.css'
+import { useState } from 'react';
+import { useEffect } from 'react';
+import styles from './App.module.css';
+import FetchDemo from './FetchDemo';
+
+
 
 function App() {
 
-    let numbers = [1, 2, 3, 4, 5]
-    const list = numbers.map((el) => <li>{el}</li>)
+    let numbers = [1, 2, 3, 4, 5, 6]
+    const list = numbers.map((number, index) => <li className={styles.listItem} key={index}>{number}</li>)
+
+    const [count, setCount] = useState(0)
+
+    //console.log(list);
+
+    // useEffect(() => {
+    //     setTimeout(() => setCount(s => s + 1), 1000)
+    // },[count])
 
     return (
-        <div>
+        <div className={styles.list}>
+            <FetchDemo/>
+
             {list}
         </div>
     )

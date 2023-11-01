9fa8df2763f666f86bf6a026b620f6df9a678b5c
Todor Borisov <todor0203@gmail.com>
2023-11-01 09:22:49 +0200

    Update Counter.jsx

--- 08. ReactJS - Oct 2023/02. Components Basic Idea/Demo 2/componetDemo/src/components/Counter.jsx
+++ 08. ReactJS - Oct 2023/02. Components Basic Idea/Demo 2/componetDemo/src/components/Counter.jsx
@@ -16,9 +16,28 @@ export default function Counter(props) {
         setClicks(0);
     };
 
+    // if (click < 0) {
+    //     return (<p> Invalid counter!</p>)
+
+    // }
+
+    // let warning = null;
+    // if (click < 0) {
+    //     warning = <p>Invalid count!</p>
+    // }
+
     return (
         <div>
             <p>{click}</p>
+
+            {/* {warning} */}
+
+            {click < 0 ? <p>Invalid count</p> : null}
+
+            {/* {click == 0 && <p>Please start incrementig!</p>}  */}
+
+
+
             <button onClick={onIncrementClick}>+</button>
             <button onClick={clear}>Clear</button>
             <button onClick={onDecrementClick}>-</button>

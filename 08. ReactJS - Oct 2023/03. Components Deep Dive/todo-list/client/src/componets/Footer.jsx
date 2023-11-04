4f4d7d053ffd1e2754b6df5d8e1236d85dbc15b3
Todor Borisov <todor0203@gmail.com>
2023-11-04 23:59:57 +0200

    Update Footer.jsx

--- 08. ReactJS - Oct 2023/03. Components Deep Dive/todo-list/client/src/componets/Footer.jsx
+++ 08. ReactJS - Oct 2023/03. Components Deep Dive/todo-list/client/src/componets/Footer.jsx
@@ -1,7 +1,7 @@
-export default function Footer(props) {
+export default function Footer() {
     return (
         <footer className="footer">
         <p>Copyright © designed by Mihail Valkov</p>
       </footer>
     );
-};
+}

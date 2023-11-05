98add8926faf57a80a20d780c8c1f0d57857053a
Todor Borisov <todor0203@gmail.com>
2023-11-05 21:03:38 +0200

    Create LikeButton.jsx

---
+++ 08. ReactJS - Oct 2023/02. Components Basic Idea/Demo 2/componentDemo/src/components/LikeButton.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import styles from './LikeButton.module.css';
+
+export default function LikeButton() {
+    const [like, setLike] = useState(false);
+    const [likesCount, setLikesCount] = useState(26);
+
+    const handleLike = () => {
+        if (!like) {
+            setLike(true);
+            setLikesCount(likesCount + 1);
+        }
+    };
+
+    return (
+
+        <div>
+            {/* <button className={like ? styles['site-btn1'] : styles['site-btn']} onClick={handleLike} disabled={like} >
+                {like ? 'Already Booked' : 'Book this boat!'}
+            </button> */}
+
+            {!like && (<button className={styles['site-btn']} onClick={handleLike}>Like this boat</button> )}
+
+            {/* <button className={like ? styles['site-btn1'] : styles['site-btn']} onClick={handleLike} disabled={like} >
+                {like ? 'Liked' : 'Like this boat'}
+            </button> */}
+           
+            <p><i className="fa-solid fa-heart" style={{color: '#ff0000'}}></i> {likesCount}</p>
+        </div>
+    );
+
+}

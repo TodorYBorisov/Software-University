cbfd706e84d4af27739fbd552798fc791b544a6f
Todor Borisov <todor0203@gmail.com>
2023-11-07 22:30:35 +0200

    Create UserList.jsx

---
+++ 08. ReactJS - Oct 2023/04. Workshop Components/React-JS-User-List-Workshop-Resources/client/src/components/UserList.jsx
@@ -0,0 +1,13 @@
+import Search from "./Search";
+import UserListTable from "./UserListTable";
+
+export default function UserList() {
+    return (
+        <section className="card users-container">
+            <Search />
+
+            <UserListTable/>
+
+        </section>
+    );
+}

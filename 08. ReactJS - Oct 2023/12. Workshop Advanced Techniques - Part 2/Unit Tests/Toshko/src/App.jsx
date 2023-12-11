ceb58de6ec30877efe405efd5d40ff28683a22b9
Todor Borisov <todor0203@gmail.com>
2023-12-11 08:22:15 +0200

    Create App.jsx

---
+++ 08. ReactJS - Oct 2023/12. Workshop Advanced Techniques - Part 2/Unit Tests/Toshko/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1>Hello World</h1>
+      <button>Test</button>
+      <p>We invite you to embark on your next adventure with us.</p>
+    </>
+  );
+}
+
+export default App;

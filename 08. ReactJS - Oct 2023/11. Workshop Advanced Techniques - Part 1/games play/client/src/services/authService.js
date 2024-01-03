899603bab6109741cf9ff3905a7e7d7584967a03
Todor Borisov <todor0203@gmail.com>
2024-01-03 20:15:44 +0200

    Create authService.js

---
+++ 08. ReactJS - Oct 2023/11. Workshop Advanced Techniques - Part 1/games play/client/src/services/authService.js
@@ -0,0 +1,20 @@
+import * as request from './requester';
+
+export const login = (email, password) => request.post('/users/login', { email, password });
+
+
+export const logout = async (accessToken) => {
+    try {
+        const response = await fetch('/users/logout', {
+            headers: {
+                'X-Authorization': accessToken
+            }
+        });
+
+        return response;
+    } catch (error) {
+        console.error(error.message);
+    }
+};
+
+export const register = (email, password) => request.post('/users/register', { email, password });

0791e4c431201b7ef9a097aa807f58e692ee7ec6
Todor Borisov <todor0203@gmail.com>
2024-01-04 11:22:12 +0200

    Create gameService.js

---
+++ 08. ReactJS - Oct 2023/11. Workshop Advanced Techniques - Part 1/games play/client/src/services/gameService.js
@@ -0,0 +1,13 @@
+import * as request from './requester';
+
+const URL = '/data/games';
+
+export const getAll = () => request.get(URL);
+
+export const getById = (gameId) => request.get(`${URL}/${gameId}`);
+
+export const create = (gameData) => request.post(URL, gameData);
+
+export const edit = (gameId, gameData) => request.put(`${URL}/${gameId}`, gameData);
+
+export const remove = (gameId) => request.del(`${URL}/${gameId}`);
